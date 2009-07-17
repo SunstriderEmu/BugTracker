bf49bf629ee753ee7007a71853e654fd48e0d3d3
krz <devnull@localhost>
2009-07-17 22:11:31 +0200

    Fix Tidal Focus affect mask.

---
+++ sql/updates/1637_world.sql
@@ -0,0 +1,5 @@
+UPDATE `spell_affect` SET `SpellFamilyMask` = '448' WHERE `entry` = '16179';
+UPDATE `spell_affect` SET `SpellFamilyMask` = '448' WHERE `entry` = '16214';
+UPDATE `spell_affect` SET `SpellFamilyMask` = '448' WHERE `entry` = '16215';
+UPDATE `spell_affect` SET `SpellFamilyMask` = '448' WHERE `entry` = '16216';
+UPDATE `spell_affect` SET `SpellFamilyMask` = '448' WHERE `entry` = '16217';
