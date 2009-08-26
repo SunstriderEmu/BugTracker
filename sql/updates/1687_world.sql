42ff1742575a5d3a8424179892d916eb405b2d73
krz <devnull@localhost>
2009-08-26 03:20:15 +0200

    Improved Poisons spell_affect_mask . Author Schaka

---
+++ sql/updates/1687_world.sql
@@ -0,0 +1,19 @@
+-- (14113) Improved Poisons (Rank 1)
+DELETE FROM `spell_affect` WHERE `entry` IN (14113);
+INSERT INTO `spell_affect` VALUES (14113, 0, 0x000000101001E000);
+
+-- (14114) Improved Poisons (Rank 2)
+DELETE FROM `spell_affect` WHERE `entry` IN (14114);
+INSERT INTO `spell_affect` VALUES (14114, 0, 0x000000101001E000);
+
+-- (14115) Improved Poisons (Rank 3)
+DELETE FROM `spell_affect` WHERE `entry` IN (14115);
+INSERT INTO `spell_affect` VALUES (14115, 0, 0x000000101001E000);
+
+-- (14116) Improved Poisons (Rank 4)
+DELETE FROM `spell_affect` WHERE `entry` IN (14116);
+INSERT INTO `spell_affect` VALUES (14116, 0, 0x000000101001E000);
+
+-- (14117) Improved Poisons (Rank 5)
+DELETE FROM `spell_affect` WHERE `entry` IN (14117);
+INSERT INTO `spell_affect` VALUES (14117, 0, 0x000000101001E000);
