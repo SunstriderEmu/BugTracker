6bfae1903f8b3d940689009888a23d06fd1a78ac
DrThum <DrThum@users.noreply.github.com>
2009-09-26 19:39:03 +0200

    Low level priest quests that require to heal and buff a npc are now working (5 quests for 5 races)

---
+++ sql/updates/1738_world_quest_priest_healAndBuff.sql
@@ -0,0 +1,14 @@
+-- human quest, entry 5624
+UPDATE quest_template set reqSpellCast1 = 2052, reqSpellCast2 = 1243, reqCreatureorGOId2 = 12423, reqCreatureOrGOCount2 = 1 WHERE entry = 5624;
+
+-- dwarf quest, entry 5625
+UPDATE quest_template set reqSpellCast1 = 2052, reqSpellCast2 = 1243, reqCreatureorGOId2 = 12427, reqCreatureOrGOCount2 = 1 WHERE entry = 5625;
+
+-- night elf quest, entry 5621
+UPDATE quest_template set reqSpellCast1 = 2052, reqSpellCast2 = 1243, reqCreatureorGOId2 = 12429, reqCreatureOrGOCount2 = 1 WHERE entry = 5621;
+
+-- troll quest, entry 5648
+UPDATE quest_template set reqSpellCast1 = 2052, reqSpellCast2 = 1243, reqCreatureorGOId2 = 12430, reqCreatureOrGOCount2 = 1 WHERE entry = 5648;
+
+-- undead quest, entry 5650
+UPDATE quest_template set reqSpellCast1 = 2052, reqSpellCast2 = 1243, reqCreatureorGOId2 = 12428, reqCreatureOrGOCount2 = 1 WHERE entry = 5650;
