26d265481d9fd90e23b8d0dd7aab2bb0722896fd
DrThum <DrThum@users.noreply.github.com>
2009-10-18 18:54:46 +0200

    [TC1 1738] Fix Alterac Valley Trinket Spells (22563, 22564) and prevent cast before BG starts

---
+++ sql/updates/1778_world_alterac_trinket.sql
@@ -0,0 +1,3 @@
+DELETE FROM spell_target_position WHERE `id` IN (22563, 22564);
+INSERT INTO spell_target_position () VALUES (22563, 30, -1347.04, -291.15, 90.8, 1.82);
+INSERT INTO spell_target_position () VALUES (22564, 30, 656.197, -30.75, 49.1, 0.24);
