cf28551af458c943fd594ce5e794bc4e0e3974e1
DrThum <DrThum@users.noreply.github.com>
2009-10-26 09:10:28 +0100

    Fix quest 10514

---
+++ sql/updates/1799_world_quest_10514.sql
@@ -0,0 +1,2 @@
+INSERT INTO spell_script_target VALUES(36652, 0, 184701); -- not required, but prevent console spamming
+UPDATE item_template SET scriptname = "item_tuber_whistle" WHERE entry = 30462;
