782743aed743d8af080b130c53ccf8b58ce92426
DrThum <DrThum@users.noreply.github.com>
2009-10-30 16:10:59 +0100

    Fix quests 10512 and 10545 (same but for both factions) + missing ScriptName for quest 1960

--- sql/updates/1821_world_quest_1960.sql
+++ sql/updates/1821_world_quest_1960.sql
@@ -1,1 +1,2 @@
 UPDATE item_template SET spellid_1 = 0, spellcooldown_1 = 0, spellcategorycooldown_1 = 0 WHERE entry = 7308;
+UPDATE item_template SET scriptname = "item_cantation_manifestation" WHERE entry = 7308;
---
+++ sql/updates/1822_world_quests_10512_10545.sql
@@ -0,0 +1,2 @@
+UPDATE quest_template SET reqspellcast1 = 0 WHERE entry = 10545;
+UPDATE item_template SET scriptname = "item_bloodmaul_keg" WHERE entry IN (29443, 30353);
