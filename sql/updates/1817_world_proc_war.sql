5b48d0ed6713a098a0c6993ace2ad027ca4aa046
DrThum <DrThum@users.noreply.github.com>
2009-10-30 11:13:24 +0100

    Modify ProcPerMinute of Stormherald and Mace Spec (5 ranks)

---
+++ sql/updates/1817_world_proc_war.sql
@@ -0,0 +1,6 @@
+UPDATE item_template SET spellppmRate_1 = 0.5 WHERE entry = 28442;
+UPDATE spell_proc_event SET ppmRate = 0.19951 WHERE entry = 12284;
+UPDATE spell_proc_event SET ppmRate = 0.39902 WHERE entry = 12701;
+UPDATE spell_proc_event SET ppmRate = 0.59853 WHERE entry = 12702;
+UPDATE spell_proc_event SET ppmRate = 0.79804 WHERE entry = 12703;
+UPDATE spell_proc_event SET ppmRate = 0.99756 WHERE entry = 12704;
