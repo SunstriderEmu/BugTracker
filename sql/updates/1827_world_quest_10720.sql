141f3bf4ae018d7cf0032bacdff19090395d3eeb
DrThum <DrThum@users.noreply.github.com>
2009-10-30 22:28:49 +0100

    Partial script for quest 10720. Possess marmot is not possible for now (GO's are too far away from where the player stands and does not appear, require core support I suppose), basic script is in SpellEffects.cpp (commented out).

---
+++ sql/updates/1827_world_quest_10720.sql
@@ -0,0 +1,3 @@
+UPDATE gameobject_template SET scriptname = "go_green_spot_grog_keg" WHERE entry = 185206;
+UPDATE gameobject_template SET scriptname = "go_ripe_moonshine_keg" WHERE entry = 185213;
+UPDATE gameobject_template SET scriptname = "go_fermented_seed_beer_keg" WHERE entry = 185214;
