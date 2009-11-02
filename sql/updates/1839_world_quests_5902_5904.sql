ee34b810952bcba756cd7aafeec965e2660633f4
DrThum <DrThum@users.noreply.github.com>
2009-11-02 10:25:44 +0100

    Spawn required GO to complete quests 5902 (Horde) and 5904 (Alliance)

---
+++ sql/updates/1839_world_quests_5902_5904.sql
@@ -0,0 +1,2 @@
+-- Permanently spawn gameobject to complete quest and give the sequel
+INSERT INTO gameobject(id, map, spawnMask, position_x, position_y, position_z, orientation, rotation0, rotation1, rotation2, rotation3, spawntimesecs, animprogress, state) VALUES(177491, 0, 1, 2449.51, -1662.48, 104.37, 5.82938, 0, 0, 0.224959, -0.974368, 25, 0, 1);
