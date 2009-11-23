2e881663fdfbcff3918e575c217db5fd5c55d7a9
DrThum <DrThum@users.noreply.github.com>
2009-11-24 00:59:38 +0100

    Fix a possible bug abuse to pull mechanar's first boss through a wall (original values of waypoints and movement type are in sql comments)

---
+++ sql/updates/1912_world_mechafirstboss.sql
@@ -0,0 +1,6 @@
+-- Original values: 210.416, 15.6408, -2.19211, 3.23112
+UPDATE creature SET position_x = 222.733627, position_y = -16.325399, position_z = -2.192110, orientation = 3.231122 WHERE id = 19219;
+-- Original value: 1
+UPDATE creature_template SET movementtype = 0 WHERE entry = 19219;
+-- Original value: 2
+UPDATE creature SET movementtype = 0 WHERE id = 19219;
