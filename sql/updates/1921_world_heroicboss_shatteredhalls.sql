69cc5a7e0960731d32f45d41e553f4d89144b301
DrThum <DrThum@users.noreply.github.com>
2009-11-25 11:59:43 +0100

    Add creature 20923 in Shattered Halls heroic

---
+++ sql/updates/1921_world_heroicboss_shatteredhalls.sql
@@ -0,0 +1,4 @@
+INSERT INTO creature(id, map, spawnMask, modelid, equipment_id, position_x, position_y, position_z, orientation, spawntimesecs, spawndist, currentwaypoint, curhealth, curmana, DeathState, MovementType) VALUES
+(20923, 540, 2, 0, 0, 481.755, 315.709, 1.9402, 3.15285, 43200, 0, 0, 139482, 0, 0, 0),
+(17427, 540, 2, 0, 0, 480.924, 311.303, 1.94235, 3.02012, 7200, 0, 0, 20283, 0, 0, 0),
+(17427, 540, 2, 0, 0, 480.636, 320.295, 1.94043, 3.58482, 7200, 0, 0, 20283, 0, 0, 0);
