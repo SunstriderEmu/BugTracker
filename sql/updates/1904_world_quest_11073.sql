afce36154784192f20564cc4b30ed2e0abba9b77
DrThum <DrThum@users.noreply.github.com>
2009-11-26 12:44:49 +0100

    Typo in sql update

--- sql/updates/1904_world_quest_11073.sql
+++ sql/updates/1904_world_quest_11073.sql
@@ -1,2 +1,2 @@
 UPDATE gameobject_template SET scriptname = "go_ancient_skull_pile" WHERE entry = 185928;
-UPDATE eventai_scripts SET creature_id = 21836 WHERE creature_id = 21838 AND comment LIKE "%divine shield%"; --temp remove of this cast, as blue ray is not spawned to break his shield
+UPDATE eventai_scripts SET creature_id = 21836 WHERE creature_id = 21838 AND comment LIKE "%divine shield%"; -- temp remove of this cast, as blue ray is not spawned to break his shield
