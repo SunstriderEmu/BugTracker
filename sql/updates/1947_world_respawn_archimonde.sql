90c7fb6422ca477a87619a89629346d8aa728424
DrThum <DrThum@users.noreply.github.com>
2009-12-04 11:52:07 +0100

    SQL to respawn Archimonde

---
+++ sql/updates/1947_world_respawn_archimonde.sql
@@ -0,0 +1,3 @@
+INSERT INTO creature(id, map, spawnMask, modelid, equipment_id, position_x, position_y, position_z, orientation, spawntimesecs, spawndist, currentwaypoint, curhealth, curmana, DeathState, MovementType) VALUES
+(17968, 534, 1, 0, 0, 5641.96, -3445.43, 1586.35, 3.66389, 604800, 0, 0, 4552500, 677400, 0, 0);
+
