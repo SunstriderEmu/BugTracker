5f6ad68c1aa0844369c349c822ff27d5b68ad9a9
DrThum <DrThum@users.noreply.github.com>
2009-12-06 23:57:24 +0100

    Daily Heroic and Battleground quests are now sharable (need client cache clear)

---
+++ sql/updates/1957_world_sharable_quests.sql
@@ -0,0 +1,3 @@
+UPDATE quest_template SET QuestFlags = 4232 WHERE entry IN (SELECT quest FROM creature_questrelation WHERE id = 24369);
+UPDATE quest_template SET QuestFlags = 4106 WHERE entry IN (11335, 11336, 11338, 11339, 11340, 11342);
+UPDATE quest_template SET QuestFlags = 4106 WHERE entry IN (11337, 11341);
