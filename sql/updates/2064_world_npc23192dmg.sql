7079a85fc0a7334e9d4f0fac93280ee9d9748cf8
DrThum <DrThum@users.noreply.github.com>
2010-02-08 23:17:37 +0100

    Fixed NPC 23192 damages

---
+++ sql/updates/2064_world_npc23192dmg.sql
@@ -0,0 +1,1 @@
+UPDATE `creature_template` SET `mindmg` = 85, `maxdmg` = 115, `attackpower` = ROUND((`mindmg` + `maxdmg`) / 4 * 7), `mindmg` = ROUND(`mindmg` - `attackpower` / 7), `maxdmg` = ROUND(`maxdmg` - `attackpower` / 7) WHERE `entry` = 23192;
