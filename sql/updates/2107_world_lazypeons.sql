050ae431985b70cde34cd62a953668c311b1ea57
DrThum <DrThum@users.noreply.github.com>
2010-03-28 17:56:25 +0200

    Added script for quest Lazy Peons, based on TC2

---
+++ sql/updates/2107_world_lazypeons.sql
@@ -0,0 +1,2 @@
+INSERT INTO script_texts (entry, content_default, content_loc2, type, comment) VALUES (-1645010, "Ow! OK, I'll get back to work, $N!", "Ow! D'accord, je retourne au travail, $N!", 1, "Quest Lazy Peons");
+UPDATE creature_template SET scriptname = "npc_lazy_peon" WHERE entry = 10556;
