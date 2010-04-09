ca9dff2287e08a1c01b968d9644cc2563e57120f
DrThum <DrThum@users.noreply.github.com>
2010-04-09 20:51:27 +0200

    Changed graveyard for all coilfang instances

---
+++ sql/updates/2130_world.sql
@@ -0,0 +1,6 @@
+DELETE FROM game_graveyard_zone WHERE ghost_zone IN (3607, 3715, 3716, 3717);
+INSERT INTO game_graveyard_zone VALUES
+(969, 3607, 0),
+(969, 3715, 0),
+(969, 3716, 0),
+(969, 3717, 0);
