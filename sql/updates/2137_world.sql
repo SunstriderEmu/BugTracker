1663aab59a6d54e6edce663571ea3b8a1383deac
DrThum <DrThum@users.noreply.github.com>
2010-04-17 20:21:40 +0200

    Fixed quest 10607

---
+++ sql/updates/2137_world.sql
@@ -0,0 +1,2 @@
+UPDATE creature_template SET scriptname = "npc_grishna_falconwing" WHERE entry = 19988;
+UPDATE creature_template SET scriptname = "npc_prophecy_questcredit" WHERE entry IN (22798, 22799, 22800, 22801);
