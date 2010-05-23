896675881af9495f558bd6a2444602ab580dcb1c
DrThum <DrThum@users.noreply.github.com>
2010-05-23 20:53:55 +0200

    Fixed typo in 2172_world.sql

--- sql/updates/2172_world.sql
+++ sql/updates/2172_world.sql
@@ -1,7 +1,6 @@
 -- Areatriggers
 DELETE FROM `areatrigger_scripts` WHERE `entry`=4016;
-INSERT INTO `areatrigger_scripts` (`entry`,`ScriptName`) VALUES (4016,'at_malfur
-ion_stormrage');
+INSERT INTO `areatrigger_scripts` (`entry`,`ScriptName`) VALUES (4016,'at_malfurion_stormrage');
 
 UPDATE `instance_template` SET `script`='instance_sunken_temple' WHERE `map`=109;
 UPDATE `gameobject_template` SET `ScriptName`='go_atalai_statue' WHERE `entry` IN (148830,148831,148832,148833,148834,148835);
