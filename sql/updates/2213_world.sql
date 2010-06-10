94a0c65cd86687451115112651497144d0656841
DrThum <DrThum@users.noreply.github.com>
2010-06-10 13:39:59 +0200

    [Oregon] Implemented spell 1852, Silenced. Not used anywhere atm.

---
+++ sql/updates/2213_world.sql
@@ -0,0 +1,1 @@
+REPLACE INTO `trinity_string` (`entry`,`content_default`, `content_loc2`) VALUES ('6616','You are silenced and can only speak with a GM.', 'Vous êtes rendu muet et ne pouvez parler qu''avec un MJ');
