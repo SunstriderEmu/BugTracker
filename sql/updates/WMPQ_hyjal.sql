fc90383f9b7f03d96100a143f582d646b7cbb3a1
DrThum <DrThum@users.noreply.github.com>
2010-06-12 00:37:49 +0200

    imported patch hyjal

---
+++ sql/updates/WMPQ_hyjal.sql
@@ -0,0 +1,4 @@
+UPDATE creature_template SET minhealth = 144624, maxhealth = 144624 WHERE entry = 17864; -- Doomguard (Wowhead's values)
+UPDATE creature_template SET minhealth = 58684, maxhealth = 83835 WHERE entry = 17905; -- Banshee (Wowhead's values)
+
+UPDATE creature_template SET mechanic_immune_mask = 650854235 WHERE entry = 17968; -- Cannot interrupt Archimonde's cast anymore
