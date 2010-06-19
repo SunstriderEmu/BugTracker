1e156075c5bd4408cf70c4e51abae60b46302f75
DrThum <DrThum@users.noreply.github.com>
2010-06-19 12:35:25 +0200

    Fixed Ward Keepers in Razorfen Kraul

---
+++ sql/updates/2299_world.sql
@@ -0,0 +1,2 @@
+DELETE FROM eventai_scripts WHERE creature_id = 4427;
+INSERT INTO eventai_scripts VALUES (442701, 4427,6, 0, 100, 2, 0, 0, 0, 0, 34, 1, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Razorfen Kraul - Ward Keepers");
