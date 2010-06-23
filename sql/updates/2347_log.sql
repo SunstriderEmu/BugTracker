b1d030c47229a3920f46822d54342ca312772a28
SUN-Leekie <dav.lebrun@gmail.com>
2010-06-23 23:27:22 +0200

    Added gmlevel into GM commands log

--- sql/updates/2347_log.sql
+++ sql/updates/2347_log.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS `gm_command`;
 CREATE TABLE `gm_command` (
   `id` int NOT NULL auto_increment,
   `account` int NOT NULL default 0,
+  `gmlevel` int NOT NULL default 0,
   `time` int NOT NULL default 0,
   `map` int NOT NULL default 0,
   `selection` int NOT NULL default 0,
