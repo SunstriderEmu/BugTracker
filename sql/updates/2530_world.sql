a5c5b5cd14b1901cadfa064da8b58b5e54716c34
DrThum <DrThum@users.noreply.github.com>
2010-09-26 20:39:07 +0200

    Zul'Aman: Link some mobs at the beginning of the instance

---
+++ sql/updates/2530_world.sql
@@ -0,0 +1,3 @@
+UPDATE creature SET pool_id = 3 WHERE id = 23597 AND position_x LIKE "138%";
+UPDATE creature SET pool_id = 3 WHERE id = 23597 AND position_x LIKE "101%";
+UPDATE creature SET pool_id = 3 WHERE id = 23889 AND pool_id = 0;
