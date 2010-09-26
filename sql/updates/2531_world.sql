d973d6191c4a1df4ef8802fa39eefff635e45630
DrThum <DrThum@users.noreply.github.com>
2010-09-26 21:42:31 +0200

    Removed a wrong spell handling + Added Akil'zon door handling

---
+++ sql/updates/2531_world.sql
@@ -0,0 +1,2 @@
+INSERT INTO gameobject (id, map, spawnmask, position_x, position_y, position_z, orientation, rotation2, rotation3, spawntimesecs, state) VALUES
+(186858, 568, 1, 336.134, 1395.48, 74.5457, 0.321584, 0.1601, 0.987101, 25, 1);
