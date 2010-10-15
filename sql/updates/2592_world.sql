8080546a513b4d332db87e28125f3fb0c6ebe91a
DrThum <DrThum@users.noreply.github.com>
2010-10-15 21:31:55 +0200

    Removed useless lines in previous sql

--- sql/updates/2592_world.sql
+++ sql/updates/2592_world.sql
@@ -1,6 +1,4 @@
-UPDATE `creature_template` SET `ScriptName` = 'npc_death_ravager' WHERE `entry` = '17556';
 UPDATE `creature_template` SET `ScriptName` = 'npc_stillpine_capitive' WHERE `entry` = '17375';
-UPDATE `gameobject_template` SET `ScriptName` = 'go_ravager_cage' WHERE `entry` = '181849';
 UPDATE `gameobject_template` SET `ScriptName` = 'go_bristlelimb_cage' WHERE `entry` = '181714';
 DELETE FROM `script_texts` WHERE `entry` BETWEEN '-1600476' AND '1600474';
 INSERT INTO `script_texts` VALUES ('-1600474', '[Fulborg] The Stillpine furbolgs will not soon forget your bravery!', null, '[Furbolg] Les Furbolgs Calmepins n''oublieront pas de si tôt votre bravoure !', null, null, null, null, null, null, '0', '0', '0', '0', 'Stillpine Capitive free say text 1');
