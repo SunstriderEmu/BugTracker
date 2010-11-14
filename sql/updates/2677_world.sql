767d248f2609957877ae2a78e0a811c48c3da57b
DrThum <DrThum@users.noreply.github.com>
2010-11-14 16:20:11 +0100

    Fixed Gorefiend ghost action bar && shadowy constructs speed

---
+++ sql/updates/2677_world.sql
@@ -0,0 +1,3 @@
+UPDATE creature_template SET speed = 1 WHERE entry = 23111;
+
+UPDATE creature_template SET spell1 = 2, spell2 = 40157, spell3 = 40175, spell4 = 40314, spell5 = 2, spell6 = 40322, spell7 = 2, spell8 = 2 WHERE entry = 23109;
