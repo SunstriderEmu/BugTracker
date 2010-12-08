8d9f962deb7a85f9b319b22c4a346c7fde600ffd
DrThum <DrThum@users.noreply.github.com>
2010-12-08 19:09:46 +0100

    Sunwell Plateau/Misc: Fixed some sentences to fit BigWigs triggers

---
+++ sql/updates/2862_world.sql
@@ -0,0 +1,2 @@
+UPDATE script_texts SET content_loc2 = "Ah, encore des agneaux pour l'abattoir !" WHERE entry = -1580022;
+UPDATE script_texts SET content_loc2 = "Aarghh !! Je ne serai plus jamais l'esclave de Malygos ! Osez me défier et vous serez détruits !" WHERE entry = -1580000;
