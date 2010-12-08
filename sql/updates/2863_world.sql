892316f712f6433746f5dc008ab4093d1033c251
DrThum <DrThum@users.noreply.github.com>
2010-12-08 20:15:59 +0100

    Sunwell Plateau/Felmyst: Correctly handle Deep Breath emote. TODO: Make it visible at much higher distance

---
+++ sql/updates/2863_world.sql
@@ -0,0 +1,1 @@
+INSERT INTO script_texts (entry, content_default, content_loc2, sound, type, language, comment) VALUES (-1580110, "takes a deep breath.", "prend une profonde respiration.", 0, 3, 0, "Felmyst - EMOTE_DEEP_BREATH");
