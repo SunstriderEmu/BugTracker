525364fc4ffd2c1b065075496b70d84cb33ffd0e
DrThum <DrThum@users.noreply.github.com>
2010-12-21 18:10:05 +0100

    Fixed quest 10997

---
+++ sql/updates/2972_world.sql
@@ -0,0 +1,2 @@
+DELETE FROM eventai_scripts WHERE id = 2219902;
+INSERT INTO eventai_scripts VALUES (2219902, 22199, 6, 0, 100, 0, 0, 0, 0, 0, 104, 185574, 60000, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Slaag - Summon Standard Chest on Death");
