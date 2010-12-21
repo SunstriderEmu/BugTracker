c551a71e21a01fd03bbe27aa3e476624c19a03b0
DrThum <DrThum@users.noreply.github.com>
2010-12-21 17:50:53 +0100

    Fixed Gameobjects spawned by creatures at death + Fixed quest 10995

---
+++ sql/updates/2970_world.sql
@@ -0,0 +1,2 @@
+INSERT INTO eventai_scripts VALUES (2021604, 20216, 6, 0, 100, 0, 0, 0, 0, 0, 104, 185567, 60000, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Grulloc - Spawn Dragon Skull at death");
+DELETE FROM creature_loot_template WHERE entry = 20216 AND item = 32379;
