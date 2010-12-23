4a015f4241c3714e600105cb203c3eeb5795fafa
DrThum <DrThum@users.noreply.github.com>
2010-12-23 02:04:36 +0100

    Missing SQL update again

---
+++ sql/updates/2979_world.sql
@@ -0,0 +1,4 @@
+UPDATE creature_template SET scriptname = "npc_soulgrinder", flags_extra = 130 WHERE entry = 23019;
+UPDATE creature_template SET scriptname = "npc_skulloc" WHERE entry = 22910;
+UPDATE creature_template SET scriptname = "npc_sundered_ghost", minhealth = 12000, maxhealth = 12200, faction_h = 14, faction_a = 14, minlevel = 70, maxlevel = 70 WHERE entry = 24039;
+UPDATE creature_template SET flags_extra = 130 WHERE entry = 23037;
