ed0ea0fccd272169cdfd1919bfb3a7635482e4ad
DrThum <DrThum@users.noreply.github.com>
2010-12-23 02:02:49 +0100

    Added missing gameobject

---
+++ sql/updates/2989_world.sql
@@ -0,0 +1,2 @@
+INSERT INTO gameobject (id, map, spawnmask, position_x, position_y, position_z, orientation, rotation0, rotation1, rotation2, rotation3, spawntimesecs, animprogress, state)
+VALUES (185919, 530, 1, 2943.9, 7066.75, 370.518, 3.49594, 0, 0, 0.984346, -0.176247, 25, 0, 1);
