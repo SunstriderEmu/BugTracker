9c6fd679294a56ed0fdf820ebb873ab847f4772d
DrThum <DrThum@users.noreply.github.com>
2010-12-25 01:13:30 +0100

    Script for quest 11065

---
+++ sql/updates/3005_world.sql
@@ -0,0 +1,2 @@
+UPDATE creature_template SET inhabitType = 4, scriptname = "npc_aether_ray" WHERE entry = 22181;
+UPDATE creature_template SET scriptname = "npc_wrangled_aether_ray" WHERE entry = 23343;
