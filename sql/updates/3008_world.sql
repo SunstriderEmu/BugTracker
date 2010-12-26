05c986e10c432b62ba629790786ff899144df31c
DrThum <DrThum@users.noreply.github.com>
2010-12-26 12:56:48 +0100

    Fixed quests 11023 and 11066 reputation reward

---
+++ sql/updates/3008_world.sql
@@ -0,0 +1,2 @@
+UPDATE quest_template SET RewRepFaction1 = 1038, RewRepValue1 = 500, RewRepFaction2 = 1031, RewRepValue2 = 500 WHERE entry = 11023;
+UPDATE quest_template SET RewRepFaction1 = 1038, RewRepValue1 = 350, RewRepFaction2 = 1031, RewRepValue2 = 350 WHERE entry = 11066;
