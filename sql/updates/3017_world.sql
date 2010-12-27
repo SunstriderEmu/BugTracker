951b7be4d224abaa190c03c27c4f24595389408d
DrThum <DrThum@users.noreply.github.com>
2010-12-27 18:38:24 +0100

    Basic Placeholders for Shartuul event

---
+++ sql/updates/3017_world.sql
@@ -0,0 +1,13 @@
+UPDATE creature_template SET flags_extra = 130 WHERE entry IN (23059, 23260, 23312, 23313, 23328, 23500);
+
+UPDATE creature_template SET faction_a = 90, faction_h = 90 WHERE entry IN (23055, 23113);
+
+DELETE FROM creature WHERE id IN (23055, 23113, 23116);
+INSERT INTO creature (id, map, spawnmask, position_x, position_y, position_z, orientation, spawntimesecs) VALUES
+(23055, 530, 1, 2740.24, 7130.62, 365.841, 2.40912, 1800),	-- 30 mins but will be respawned by event
+(23113, 530, 1, 2720.11, 7117.68, 367.31, 2.70509, 1800),
+(23116, 530, 1, 2720.11, 7117.68, 367.31, 2.70509, 1800);
+
+UPDATE creature_template SET scale = 5, flags_extra = 130 WHERE entry = 23116;
+DELETE FROM creature_template_addon WHERE entry = 23116;
+INSERT INTO creature_template_addon (entry, auras) VALUES (23116, "40158 0");
