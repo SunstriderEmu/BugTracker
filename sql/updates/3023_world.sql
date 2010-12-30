2f53c5aac93edd5af348af3ca35713ea040ffe30
DrThum <DrThum@users.noreply.github.com>
2010-12-30 11:32:33 +0100

    Implemented possibility to enable/disable rated arena from config (error message is trinity_string #751)

---
+++ sql/updates/3023_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM trinity_string WHERE entry = 751;
+INSERT INTO trinity_string (entry, content_default, content_loc2) VALUES
+(751, "Rated arena is disabled for now. It will be back on Jan 12th, prepare yourself!", "La saison 1 étant terminée, les arènes cotées sont désactivées pour le moment. Elles seront de retour le 12 janvier pour la saison 2. Soyez prêts !");
