90ad1ec7c5fbb8e0bfa72a69d964ab831f4c1ae2
DrThum <DrThum@users.noreply.github.com>
2011-01-07 14:33:50 +0100

    Implemented script for quest 11166

---
+++ sql/updates/3055_world.sql
@@ -0,0 +1,2 @@
+DELETE FROM areatrigger_scripts WHERE entry IN (4724, 4725, 4726);
+INSERT INTO areatrigger_scripts VALUES (4724, "at_quest_X_marks"), (4725, "at_quest_X_marks"), (4726, "at_quest_X_marks");
