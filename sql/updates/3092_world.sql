f010129c5edc35dd1d282a157f99e2c66ec6624b
DrThum <DrThum@users.noreply.github.com>
2011-01-13 13:14:44 +0100

    Stratholme/Rammstein: now yells DU HAST when pulled :D

---
+++ sql/updates/3092_world.sql
@@ -0,0 +1,2 @@
+DELETE FROM script_texts WHERE entry = -1043900;
+INSERT INTO script_texts (entry, content_default, content_loc2, type, comment) VALUES (-1043900, "Du hast!", "Du hast!", 1, "Rammstein the Gorger - SAY_DUHAST");
