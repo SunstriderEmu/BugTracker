bafd92cd4528d135765eab590df6b986cb7c69f1
DrThum <DrThum@users.noreply.github.com>
2011-01-17 20:49:05 +0100

    Fixed quests 10658, 10654, 10326, 10415 Sha'tar rep reward

---
+++ sql/updates/3117_world.sql
@@ -0,0 +1,2 @@
+UPDATE quest_template SET RewRepValue2 = -375, RewRepFaction3 = 935, RewRepValue3 = 175 WHERE entry = 10421;
+UPDATE quest_template SET RewRepFaction3 = 935, RewRepValue3 = 125 WHERE entry IN (10658, 10654, 10326, 10415);
