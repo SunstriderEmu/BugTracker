6da4029724acd59a413fd4f8ed5dc3544610f264
DrThum <DrThum@users.noreply.github.com>
2011-04-15 16:57:15 +0200

    Fixed npc 1313 items amount

---
+++ sql/updates/3181_world.sql
@@ -0,0 +1,2 @@
+UPDATE npc_vendor SET maxcount = 3, incrtime = 7200 WHERE entry = 1313 AND item IN (765, 2447);
+UPDATE npc_vendor SET maxcount = 1, incrtime = 7200 WHERE entry = 1313 AND item IN (3357)
