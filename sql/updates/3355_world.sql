c742a978a39d81636f675ae72a64abe40bd311e5
DrThum <DrThum@users.noreply.github.com>
2011-06-12 11:17:15 +0200

    Commented not ready SQL updates

--- sql/updates/3355_world.sql
+++ sql/updates/3355_world.sql
@@ -1,3 +1,3 @@
 -- Don't apply for now
-UPDATE creature_template SET AIName = "ArcherAI" WHERE entry IN (13358, 13359);
-UPDATE creature_model_info SET combat_reach = 60 WHERE modelid IN (13390, 13393);
+-- UPDATE creature_template SET AIName = "ArcherAI" WHERE entry IN (13358, 13359);
+-- UPDATE creature_model_info SET combat_reach = 60 WHERE modelid IN (13390, 13393);
