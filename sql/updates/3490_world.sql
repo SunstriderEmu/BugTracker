9dbe8a2d9ac957fb6740d8e098dc502d304a41d7
DrThum <DrThum@users.noreply.github.com>
2011-08-09 15:05:55 +0200

    Really fixed it

--- sql/updates/3490_world.sql
+++ sql/updates/3490_world.sql
@@ -1,3 +1,5 @@
 UPDATE creature_template SET AIName = "SmartAI" WHERE entry = 23080;
 DELETE FROM smart_scripts WHERE entryorguid = 23080;
-INSERT INTO smart_scripts VALUES  (23080, 0, 0, 0, 1, 0, 100, 3, 500, 500, 5000, 5000, 11, 40089, 3, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Cast Geyser (Knockback) when summoned");
+INSERT INTO smart_scripts VALUES  (23080, 0, 0, 0, 0, 0, 100, 3, 200, 200, 5000, 5000, 11, 40089, 3, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Cast Geyser (Knockback) when summoned"), (23080, 0, 1, 0, 0, 0, 100, 1, 500, 500, 5000, 5000, 37, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Suicide himself");
+
+UPDATE creature_template SET minlevel = 70, maxlevel = 70, faction_A = 1813, faction_H = 1813, flags_extra = 2 WHERE entry = 23080;
