b32aa73cf0b738c57f6c87a9c22e3e452c8f353b
DrThum <DrThum@users.noreply.github.com>
2011-08-10 03:42:22 +0200

    Added back TargetedMoveGen fix + some tweaks to creature 22884 EventAI

---
+++ sql/updates/3495_world.sql
@@ -0,0 +1,3 @@
+UPDATE eventai_scripts SET action1_param2 = 1, event_param1 = 10000, event_param2 = 10000, event_param3 = 12000, event_param4 = 15000 WHERE id = 2288401;
+UPDATE eventai_scripts SET event_chance = 100 WHERE creature_id = 22884;
+
