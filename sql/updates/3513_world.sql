060b96ebc5462ef93e4b148406b3500b343c13cc
DrThum <DrThum@users.noreply.github.com>
2011-08-12 18:06:14 +0200

    Fixed creature 23339 spells

---
+++ sql/updates/3513_world.sql
@@ -0,0 +1,3 @@
+UPDATE eventai_scripts SET event_type = 0, event_flags = 3, event_param1 = 5000, event_param2 = 8000, event_param3 = 20000, event_param4 = 25000, action1_param2 = 4 WHERE id = 2333909;
+UPDATE creature_template SET faction_a = 1813, faction_h = 1813 WHERE entry = 23389;
+UPDATE creature_template SET mindmg = 3233, maxdmg = 6611, attackpower = 17000 WHERE entry = 23389;
