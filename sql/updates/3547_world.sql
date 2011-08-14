59a9bbd1db2d03fa316e62502b9d160d0e2f29da
DrThum <DrThum@users.noreply.github.com>
2011-08-15 01:48:17 +0200

    Fixed script for creatures 23222, 23223, 23239

---
+++ sql/updates/3547_world.sql
@@ -0,0 +1,3 @@
+INSERT INTO eventai_scripts VALUES (2322306, 23223, 2, 0, 100, 2, 30, 0, 0, 0, 11, 8269, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Bonechewer Spectator - Cast Frenzy at 30% HP");
+INSERT INTO eventai_scripts VALUES (2322202, 23222, 2, 0, 100, 2, 30, 0, 0, 0, 11, 8269, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Bonechewer Brawler - Cast Frenzy at 30% HP");
+INSERT INTO eventai_scripts VALUES (2323902, 23239, 0, 0, 100, 3, 10000, 10000, 30000, 30000, 11, 41251, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Bonechewer Combatant - Cast Combat Rage");
