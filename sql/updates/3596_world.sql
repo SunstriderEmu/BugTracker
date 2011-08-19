10dd9213dc4bbeed48cc5a275ece8acb0f52b17a
DrThum <DrThum@users.noreply.github.com>
2011-08-19 15:38:50 +0200

    Sunwell/Twins: Despawn shadow images and remove Flame/Dark Touched in case of wipe/down + Translated emote + Fixed aggro radius

---
+++ sql/updates/3596_world.sql
@@ -0,0 +1,2 @@
+UPDATE script_texts SET content_default = "Grand warlock Alythess directs Conflagration at $N", content_loc2 = "Grande démoniste Alythess dirige Conflagration sur $N" WHERE entry = -1580065;
+UPDATE script_texts SET content_loc2 = "Dame Sacrocingle dirige Nova d'ombre sur $N", content_default = "Lady Sacrolash directs Shadow Nova at $N" WHERE entry = -1580064;
