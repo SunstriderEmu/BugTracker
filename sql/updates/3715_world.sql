bb8afba4d9685eb6b8215b02c19cb07faa92232b
DrThum <DrThum@users.noreply.github.com>
2011-09-18 22:42:04 +0200

    Implemented SPELL_AURA_MECHANIC_IMMUNITY_MASK (147) && Fixed Supremus emotes

---
+++ sql/updates/3715_world.sql
@@ -0,0 +1,2 @@
+UPDATE script_texts SET content_default = "Supremus punches the ground in anger!" WHERE entry = -1564011;
+UPDATE script_texts SET content_default = "Supremus acquires a new target!", content_loc2 = "Supremus acquiert une nouvelle cible !" WHERE entry = -1564010;
