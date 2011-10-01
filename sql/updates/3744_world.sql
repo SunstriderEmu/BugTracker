102890f331f98816bce093dcedb181a3d2226d02
DrThum <DrThum@users.noreply.github.com>
2011-10-01 19:11:50 +0200

    Sunwell: Rewritten part of Brutallus intro, almost blizzlike now

---
+++ sql/updates/3744_world.sql
@@ -0,0 +1,3 @@
+INSERT INTO spell_script_target VALUES (46637, 1, 19871);
+
+UPDATE creature_template SET minlevel = 73, maxlevel = 73, mindmg = 9500, maxdmg = 9500, attackpower = 87500 WHERE entry = 24895;
