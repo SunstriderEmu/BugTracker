6c49b93399969c17c4eba9f4ccc689ab2d8d6285
DrThum <DrThum@users.noreply.github.com>
2011-10-07 14:32:30 +0200

    Sunwell/Twins: Fixed French translation for 2 emotes - should fix BigWigs alerts

---
+++ sql/updates/3758_world.sql
@@ -0,0 +1,2 @@
+UPDATE script_texts SET content_loc2 = "Grande démoniste Alythess dirige Déflagration sur $N" WHERE entry = -1580065;
+UPDATE script_texts SET content_loc2 = "Dame Sacrocingle dirige Nova de l'ombre sur $N" WHERE entry = -1580064;
