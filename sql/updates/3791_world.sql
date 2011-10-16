06aee2e23709d4b0bcea9b3ae59f3269a0c363c6
DrThum <DrThum@users.noreply.github.com>
2011-10-16 16:55:18 +0200

    BRD: Fixed quest 4463 && item 11602 droprate

---
+++ sql/updates/3791_world.sql
@@ -0,0 +1,2 @@
+INSERT INTO creature_loot_template VALUES (9499, 11602, 100, 0, 1, 1, 0, 0, 0);
+UPDATE gameobject_template SET scriptname = "go_dark_keeper_portrait" WHERE entry IN (161456, 161457, 161458, 161459, 164818, 164819);
