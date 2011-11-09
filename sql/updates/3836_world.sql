9051221ca8d3726ddefec002b652b656be52662a
DrThum <DrThum@users.noreply.github.com>
2011-11-09 20:53:42 +0100

    Finally fixed fuckin Zul'aman chests
    Former-commit-id: 7e48037f9a623d22d6562387193d1ea08f87f323

---
+++ sql/updates/3836_world.sql
@@ -0,0 +1,3 @@
+UPDATE gameobject_loot_template SET groupid = 2 WHERE entry IN (22699, 22790, 22797, 22968) AND condition_value2 = 2;
+UPDATE gameobject_loot_template SET groupid = 3 WHERE entry IN (22699, 22790, 22797, 22968) AND condition_value2 = 3;
+UPDATE gameobject_loot_template SET groupid = 4 WHERE entry IN (22699, 22790, 22797, 22968) AND condition_value2 = 4;
