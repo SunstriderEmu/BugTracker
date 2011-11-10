9cc824ffb0548b5d4fa2d905b5a5297de895b336
DrThum <DrThum@users.noreply.github.com>
2011-11-10 17:54:53 +0100

    Fixed Mend pet && Netherspite portals buffs stacking speed
    Former-commit-id: 960fef21e0b54b1814076f58523d27f79a81cd70

---
+++ sql/updates/3839_world.sql
@@ -0,0 +1,2 @@
+UPDATE script_texts SET content_default = "Netherspite goes into a nether-fed rage!", content_loc2 = "Dédain-du-Néant sombre dans une rage venue du Néant !" WHERE entry = -1532090;
+UPDATE script_texts SET content_default = "Netherspite cries out in withdrawal, opening gates to the warp.", content_loc2 = "Dédain-du-Néant crie au loin, ouvrant les portes vers le Néant." WHERE entry = -1532089;
