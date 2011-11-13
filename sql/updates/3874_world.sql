b8f9d9b4ea19ec9482f355019f822ce9184f7aae
DrThum <DrThum@users.noreply.github.com>
2011-11-13 23:06:51 +0100

    Karazhan: Torment of the Worgen now has a 3% chance of proccing
    Former-commit-id: 80ef3aaa90ab949fbd0b3f29866d81d045a8cda2

---
+++ sql/updates/3874_world.sql
@@ -0,0 +1,2 @@
+UPDATE spell_template SET procChance = 3, effect3 = 0, effectDieSides3 = 0, effectBaseDice3 = 0, effectImplicitTargetA3 = 0, effectApplyAuraName3 = 0, effectTriggerSpell3 = 0 WHERE id = 30567;
+UPDATE spell_template SET effect2 = 6, effectDieSides2 = 1, effectBaseDice2 = 1, effectImplicitTargetA2 = 1, effectApplyAuraName2 = 42, effectTriggerSpell2 = 30731 WHERE id = 30564;
