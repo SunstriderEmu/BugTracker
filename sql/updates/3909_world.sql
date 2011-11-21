c9b7620d20db01c581753637cc23c7c222ac6603
DrThum <DrThum@users.noreply.github.com>
2011-11-21 17:29:34 +0100

    Fixed quest 9868
    Former-commit-id: c9ec0284c1ad404567485d35578ff3cda7c8825f

---
+++ sql/updates/3909_world.sql
@@ -0,0 +1,7 @@
+UPDATE script_texts SET content_loc2 = "Nous sommes encerclés !" WHERE entry = -1000482;
+UPDATE script_texts SET content_loc2 = "C'est une embuscade ! Défendez-vous !" WHERE entry = -1000483;
+UPDATE script_texts SET content_loc2 = "Nous devons partir avant d'en alerter d'autres." WHERE entry = -1000484;
+UPDATE script_texts SET content_loc2 = "Où pensez-vous aller comme ça ? Tuez les tous !" WHERE entry = -1000485;
+UPDATE script_texts SET content_loc2 = "Prends ça !" WHERE entry = -1000486;
+UPDATE script_texts SET content_loc2 = "HORION DE GIVRE !" WHERE entry = -1000487;
+UPDATE script_texts SET content_loc2 = "C'est mieux si nous nous séparons maintenant, au cas où ils en enverraient d'autres après nous. Espérons qu'un de nous parvienne à retourner auprès de Garrosh. Bonne chance, étranger." WHERE entry = -1000488;
