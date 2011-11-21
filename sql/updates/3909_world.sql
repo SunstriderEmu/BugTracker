73e1af7d50d28e3a92466162d5f26363999fd319
DrThum <DrThum@users.noreply.github.com>
2011-11-21 17:30:49 +0100

    Forgotten change in previous commit
    Former-commit-id: b203e25ae0a97cc4a37b79724d57dd20ea0e70cc

--- sql/updates/3909_world.sql
+++ sql/updates/3909_world.sql
@@ -5,3 +5,5 @@ UPDATE script_texts SET content_loc2 = "Où pensez-vous aller comme ça ? Tuez l
 UPDATE script_texts SET content_loc2 = "Prends ça !" WHERE entry = -1000486;
 UPDATE script_texts SET content_loc2 = "HORION DE GIVRE !" WHERE entry = -1000487;
 UPDATE script_texts SET content_loc2 = "C'est mieux si nous nous séparons maintenant, au cas où ils en enverraient d'autres après nous. Espérons qu'un de nous parvienne à retourner auprès de Garrosh. Bonne chance, étranger." WHERE entry = -1000488;
+
+UPDATE creature_template SET scriptname = "npc_maghar_captive" WHERE entry = 18210;
