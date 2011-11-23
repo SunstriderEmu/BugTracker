7824ae8da38fe6ef55c5c6bebb904a9e8a15e7f7
DrThum <DrThum@users.noreply.github.com>
2011-11-23 19:10:38 +0100

    Fixed quest 10544
    Former-commit-id: 3192063f4addc8a2fdf2f74c48cc79645fbd5e6c

---
+++ sql/updates/3915_world.sql
@@ -0,0 +1,3 @@
+UPDATE gameobject_template SET data1 = 35 WHERE entry = 300104;
+UPDATE gameobject SET position_x = 1544.2099, position_y = 6321.0498, position_z = 2.3606 WHERE guid = 99968;
+UPDATE gameobject SET position_x = 1589.6099, position_y = 6294.2597, position_z = 2.9776 WHERE guid = 99967;
