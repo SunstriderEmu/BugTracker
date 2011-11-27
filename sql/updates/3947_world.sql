c78f53840789f894b6686fb3bd0b6bb8e50e6239
DrThum <DrThum@users.noreply.github.com>
2011-11-27 22:14:49 +0100

    Fixed quest 11159
    Former-commit-id: 41160df0c2c8218b874fb6aeace6a45498dcb856

---
+++ sql/updates/3947_world.sql
@@ -0,0 +1,3 @@
+UPDATE gameobject_template SET data3 = 0 WHERE entry = 186334;
+UPDATE gameobject_template SET AIName = "SmartGameObjectAI", ScriptName = "" WHERE entry = 186332;
+INSERT INTO smart_scripts VALUES (186332, 1, 0, 0, 64, 0, 100, 0, 0, 0, 0, 0, 12, 23786, 1, 30000, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "Ogre Remains - Summon Stonemaul Spirit on GO Hello");
