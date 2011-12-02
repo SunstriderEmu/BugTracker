604bc42bef9143dc960cff1e87ef75fa267daa92
DrThum <DrThum@users.noreply.github.com>
2011-12-02 16:04:55 +0100

    Fixed translations of quest 4770
    Former-commit-id: 94316a34515ed34be9e491e495fe4b4068f5a558

---
+++ sql/updates/3967_world.sql
@@ -0,0 +1,3 @@
+UPDATE script_texts SET content_loc2 = "Allons-y, $N. Je suis prêt à aller jusqu'au Poste de Blanc-relais." WHERE entry = -1000147;
+UPDATE script_texts SET content_loc2 = "Ça me semble sûr. Si nous allons vers l'est, nous allons sans doute... Aah ! Une attaque de wyvernes !" WHERE entry = -1000148;
+UPDATE script_texts SET content_loc2 = "Merci beaucoup... A partir d'ici, je devrais me débrouiller pour rentrer au Poste de Blanc-relais. Parlez à Motega Crin-de-feu, peut-être pourrez-vous le convaincre de me renvoyer chez moi." WHERE entry = -1000149;
