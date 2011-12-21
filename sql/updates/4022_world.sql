7282ecd20d376246b43e5a0348dd996da4436db1
DrThum <DrThum@users.noreply.github.com>
2011-12-21 22:28:02 +0100

    Fixed quest 10702
    Former-commit-id: 9fd121fc08c5927ca43f2ff58f3c70b83d5889f4

---
+++ sql/updates/4022_world.sql
@@ -0,0 +1,5 @@
+UPDATE creature_template SET AIName = "SmartAI", ScriptName = "" WHERE entry IN (21878, 21864, 21879, 23020);
+INSERT INTO smart_scripts VALUES (21878, 0, 0, 0, 6, 0, 100, 0, 0, 0, 0, 0, 33, 21978, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "Quest 10702 - KillCredit on death");
+INSERT INTO smart_scripts VALUES (21864, 0, 0, 0, 6, 0, 100, 0, 0, 0, 0, 0, 33, 21978, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "Quest 10702 - KillCredit on death");
+INSERT INTO smart_scripts VALUES (21879, 0, 0, 0, 6, 0, 100, 0, 0, 0, 0, 0, 33, 21978, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "Quest 10702 - KillCredit on death");
+INSERT INTO smart_scripts VALUES (23020, 0, 0, 0, 6, 0, 100, 0, 0, 0, 0, 0, 33, 21978, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "Quest 10702 - KillCredit on death");
