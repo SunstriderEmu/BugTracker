fda54e33b8b0e054966aa1953a6ea92ae5d9f91a
DrThum <DrThum@users.noreply.github.com>
2011-12-24 09:47:53 +0100

    Backed out changeset related to characters deletion
    Former-commit-id: ee07c8c8acb3078d922a50ad10dd2900b44fc2dc

--- sql/updates/4016_characters.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE characters ADD COLUMN `deleted` tinyint(3) unsigned NOT NULL DEFAULT '0' AFTER lastGenderChange;
-ALTER TABLE characters ADD COLUMN `deleted_time` bigint(20) unsigned NOT NULL DEFAULT '0' AFTER deleted;
