5e4e8082a5be811cd42c3b4fdea33e186eaab810
DrThum <DrThum@users.noreply.github.com>
2011-12-30 20:58:36 +0100

    Fixed NPC 15185 InhabitType
    Former-commit-id: d96729ba030774b9c285b13b9714b80455d99f3a

---
+++ sql/updates/4065_world.sql
@@ -0,0 +1,3 @@
+UPDATE creature_template SET InhabitType = 5 WHERE entry = 15185;
+
+UPDATE creature SET position_x = -6625.12, position_y = 72.326, position_z = 189.328 WHERE id = 15185 AND position_x = -6678.140137;
