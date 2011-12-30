4529c74a0ca0a5603b084c907c18451b3c6bb14e
DrThum <DrThum@users.noreply.github.com>
2011-12-30 22:03:54 +0100

    Paladin: Fixed Seal of the Crusader attack power bonus
    Former-commit-id: be20fb93b98921c66aa648834f73495dd12c54dd

---
+++ sql/updates/4067_world.sql
@@ -0,0 +1,7 @@
+UPDATE spell_template SET effectBasePoints1 = 31 WHERE id = 21082;
+UPDATE spell_template SET effectBasePoints1 = 50 WHERE id = 20162;
+UPDATE spell_template SET effectBasePoints1 = 94 WHERE id = 20305;
+UPDATE spell_template SET effectBasePoints1 = 150 WHERE id = 20306;
+UPDATE spell_template SET effectBasePoints1 = 236 WHERE id = 20307;
+UPDATE spell_template SET effectBasePoints1 = 332 WHERE id = 20308;
+UPDATE spell_template SET effectBasePoints1 = 453 WHERE id = 27158;
