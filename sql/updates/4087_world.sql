3beb74749cfb6eb7eb6651fb490be5605e8069b5
DrThum <DrThum@users.noreply.github.com>
2012-01-01 22:25:51 +0100

    Lunar Festival: Typo in Big white cluster smart script
    Former-commit-id: 8045b49236232645505df546e87aed5d5985877c

---
+++ sql/updates/4087_world.sql
@@ -0,0 +1,2 @@
+UPDATE smart_scripts SET source_type = 0, id = 1 WHERE entryorguid = 15915 AND source_type = 1;
+UPDATE smart_scripts SET source_type = 0, id = 2 WHERE entryorguid = 15915 AND source_type = 2;
