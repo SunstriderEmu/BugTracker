2c538cb42d0dee37f02c214db5d2b77226fbaa28
DrThum <DrThum@users.noreply.github.com>
2012-01-03 22:26:33 +0100

    Spawned npc 6490 in Scarlet Monastery (Graveyard)
    Former-commit-id: e5066e762981606673557cd9b16955987138a626

---
+++ sql/updates/4112_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM creature WHERE id = 6490;
+INSERT INTO creature (id, map, spawnMask, position_x, position_y, position_z, orientation, spawntimesecs, MovementType) VALUES
+(6490, 189, 1, 1757.9, 1322.19, 17.7809, 6.17898, 7200, 0);
