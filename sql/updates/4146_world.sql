8927f5f2eea480191e150ff4928c1b5717ee1051
DrThum <DrThum@users.noreply.github.com>
2012-01-16 21:48:17 +0100

    Chess: Fixed pieces health
    Former-commit-id: 7ac9fc47ec2e3a07ff44cdda81efc43044b4f636

---
+++ sql/updates/4146_world.sql
@@ -0,0 +1,2 @@
+UPDATE creature_template SET minhealth = 50000, maxhealth = 50000 WHERE entry IN (17211, 21664, 21682, 21683);
+UPDATE creature_template SET minhealth = 58000, maxhealth = 58000 WHERE entry = 21160;
