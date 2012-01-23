373fbcf6788097d0409aa3ac60f436ebd8c06052
DrThum <DrThum@users.noreply.github.com>
2012-01-23 19:33:22 +0100

    Translated and fixed Azuremyst quests
    Former-commit-id: 8593cc3cfef0339918ce093cace31870b213203a

---
+++ sql/updates/4158_world.sql
@@ -0,0 +1,12 @@
+UPDATE locales_quest SET RequestItemsText_loc2 = "Nous ne pouvons pas prendre le risque que cette contamination se répande. Les rapports que j'ai eus venant du Guet du sang sont assez mauvais. Cela doit s'arrêter maintenant !" WHERE entry = 9456;
+UPDATE locales_quest SET RequestItemsText_loc2 = "Vous avez fait de grandes choses pour mon peuple, $N." WHERE entry = 9513;
+UPDATE locales_quest SET RequestItemsText_loc2 = "Restez prudent !" WHERE entry = 9515;
+UPDATE locales_quest SET EndText_loc2 = "Magwin escorté en sécurité", RequestItemsText_loc2 = "Salutations, $N.", OfferRewardText_loc2 = "Des larmes de joie coulent sur le visage de Cowlen.>$B$BComment puis-je un jour vous rembourser, $N ? S'il vous plait, prenez cet héritage. Il représente un lien sacré d'amitié, de confiance et d'amour entre les elfes de la nuit. Vous êtes comme $Gun frère:une soeur; - à partir de ce jour jusqu'à la fin des temps..." WHERE entry = 9528;
+UPDATE locales_quest SET RequestItemsText_loc2 = "[Furbolg] Donc la prophétie d'Akida est vraie... Un héros a été envoyé vers nous." WHERE entry = 9544;
+UPDATE locales_quest SET OfferRewardText_loc2 = "Ceci pourrait très bien être l'opportunité dont nous avons besoin ! Je vais envoyer une attaque d'un coup ! Les Calmepins vous remercient pour votre service,$N.", RequestItemsText_loc2 = "Votre tâche est-elle complète ?" WHERE entry = 9573;
+UPDATE locales_quest SET OfferRewardText_loc2 = "Bien joué, $N. Vraiment bien, en fait ! Vous semblez être $Gun:une; $R très $Gspécal:spéciale; - plus sage que ceux de votre âge. Prenez ceci en récompense !" WHERE entry = 9612;
+UPDATE locales_quest SET OfferRewardText_loc2 = "Comme pour chaque sort, vous devez vous protéger de son attrait. et ne l'utiliser que lorsque c'est nécessaire et pour servir le bien. Espérons que le spectre vous a apporté un aperçu de ce que vous pourriez devenir si vous ne faites pas attention -- un esclave décérébré de la magie.", RequestItemsText_loc2 = "Avez-vous affronté les cauchemards." WHERE entry = 9595;
+UPDATE gameobject_template SET castBarCaption = "Ouverture" WHERE entry = 181644;
+UPDATE script_texts SET content_loc2 = "Oui maître, tout se passe comme prévu." WHERE entry = -1000256;
+UPDATE script_texts SET content_loc2 = "met la coquille dans son oreille" WHERE entry = -1000257;
+UPDATE script_texts SET content_loc2 = "Et maintenant, je vous découpe !" WHERE entry = -1000258;
