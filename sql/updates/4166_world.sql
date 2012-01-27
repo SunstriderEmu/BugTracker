e9d2dae6f87b0a6b83cfb756b0c0b20d60c829bb
DrThum <DrThum@users.noreply.github.com>
2012-01-27 15:07:11 +0100

    CreatureAINew: Fixed EVENT_FLAG_DELAY_IF_CASTING and Azuregos script
    Former-commit-id: 1cc9a72a2decd4ca8a7e46d18f299fabc01d44c0

---
+++ sql/updates/4166_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM script_texts WHERE entry = 6109;
+DELETE FROM creature_text WHERE entry = 6109;
+INSERT INTO creature_text VALUES (6109, 0, 0, "Come, little ones. Face me!", "Venez, mes petits. Affrontez-moi !", 1, 0,100, 0, 0, 0, "SAY_TELEPORT");
