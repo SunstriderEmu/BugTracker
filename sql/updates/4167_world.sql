65ff06257eaf352dc8ee7263aa67f2a0ecbd86b6
DrThum <DrThum@users.noreply.github.com>
2012-01-27 15:39:37 +0100

    Fixed all Dun Morogh quests
    Former-commit-id: 4f659ae863bd317fd761b0b1ca0333d2368ec32b

---
+++ sql/updates/4167_world.sql
@@ -0,0 +1,7 @@
+UPDATE gameobject SET spawntimesecs = 15 WHERE id = 176785;
+
+UPDATE locales_quest SET RequestItemsText_loc2 = "Cela me rend fou ! Grr..." WHERE entry = 432;
+
+UPDATE eventai_texts SET content_loc2 = "Détruire !" WHERE entry = -215;
+UPDATE eventai_texts SET content_loc2 = "Tuer !" WHERE entry = -214;
+UPDATE eventai_texts SET content_loc2 = "Ecraser !" WHERE entry = -213;
