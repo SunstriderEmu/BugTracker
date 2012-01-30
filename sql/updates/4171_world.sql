9ac1b4f5da0c696036417de7d226c7269f834bfc
DrThum <DrThum@users.noreply.github.com>
2012-01-30 11:56:26 +0100

    Added notification at login when a mail change has been requested
    Former-commit-id: e74f3d02d471fa9252982559d852987197f439d6

---
+++ sql/updates/4171_world.sql
@@ -0,0 +1,2 @@
+DELETE FROM trinity_string WHERE entry = 812;
+INSERT INTO trinity_string (entry, content_default, content_loc2) VALUES (812, "A mail change has been requested on this account. Please check that the new address is yours!", "Un changement de mail a été demandé pour ce compte. Merci de vérifier que la nouvelle adresse vous appartient !");
