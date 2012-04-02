23f141a1e7725670c5bd4f2e7b52777354cb9543
DrThum <DrThum@users.noreply.github.com>
2012-04-02 15:55:14 +0200

    Fixed quest 945
    Former-commit-id: 517a6bdd0ea71a8e5ccb42d4b33ed744b193eb52

---
+++ sql/updates/4284_world.sql
@@ -0,0 +1,3 @@
+UPDATE quest_start_scripts SET dataint = 2000000018 WHERE id = 945 AND delay = 92;
+UPDATE quest_start_scripts SET dataint = 2000000017 WHERE id = 945 AND delay = 0;
+INSERT INTO quest_start_scripts VALUES (945, 91, 7, 945, 0, 0, 0, 0, 0, 0);
