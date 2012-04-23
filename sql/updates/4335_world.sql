22e938b1878f5ad12252e6b37cf0a6db78882a9e
DrThum <DrThum@users.noreply.github.com>
2012-04-23 21:28:06 +0200

    M'uru: Added sql scriptnames
    Former-commit-id: 92ded380a2e898b2b664d9ad5c43093dac750140

---
+++ sql/updates/4335_world.sql
@@ -0,0 +1,2 @@
+DELETE FROM creature_scripts WHERE entryorguid IN (25741, 25744, 25770, 25772, 25840, 25855);
+INSERT INTO creature_scripts VALUES (25741, "boss_muru"), (25744, "npc_dark_fiend"), (25770, "npc_muru_portal"), (25772, "npc_void_sentinel"), (25840, "boss_entropius"), (25855, "npc_blackhole");
