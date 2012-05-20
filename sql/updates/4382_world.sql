44c4cde2ed30abbcef5f724da31309b6b438e0e4
DrThum <DrThum@users.noreply.github.com>
2012-05-20 19:14:57 +0200

    Fixed item 34427 internal cooldown
    Former-commit-id: 4ee8d4820943cc10f111dcd1e579118c2ddb35f2

--- sql/updates/4382_world.sql
+++ sql/updates/4382_world.sql
@@ -1,4 +1,5 @@
 UPDATE trinity_string SET content_default = "|cffff0000[Automatic]: %s|r", content_loc2 = "|cffff0000[Automatique]: %s|r" WHERE entry = 786;
 
+DROP TABLE IF EXISTS auto_ann_by_time;
 CREATE TABLE auto_ann_by_time ( id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, message TEXT NOT NULL DEFAULT '', hour INTEGER NOT NULL DEFAULT 0, minute INTEGER NOT NULL DEFAULT 0 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 INSERT INTO auto_ann_by_time VALUES ('', "A partir de 20h, session d'arène dédiée à la 3v3 !", 19, 30), ('', "La session d'arène dédiée à la 3v3 commence pour une durée de 2 heures !", 20, 00);
---
+++ sql/updates/4383_world.sql
@@ -0,0 +1,2 @@
+DELETE FROM spell_proc_event WHERE entry = 45355;
+INSERT INTO spell_proc_event (entry, cooldown) VALUES (45355, 45);
