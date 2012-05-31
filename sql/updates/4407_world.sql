195fd656404e7e397817033630dd5b982e26157b
SUN-Leekie <mathman.pro@gmail.com>
2012-05-31 18:39:52 +0200

    Script/M'uru: - Add NullCreatureAI on CreatureAiNew
    Former-commit-id: b93770bea5101b7fc5b6574a5ea38ecbbae6a691

---
+++ sql/updates/4407_world.sql
@@ -0,0 +1,20 @@
+/* Void Spawn */
+UPDATE creature_template SET AIName = "NullCreatureAI" where entry = 25824;
+
+/* Entropius */
+UPDATE creature_template SET AIName = "NullCreatureAI" where entry = 25840;
+
+/* M'uru portal */
+UPDATE creature_template SET AIName = "NullCreatureAI" where entry = 25770;
+
+/* Dark fiend */
+UPDATE creature_template SET AIName = "NullCreatureAI" where entry = 25744;
+
+/* Void sentinel */
+UPDATE creature_template SET AIName = "NullCreatureAI" where entry = 25772;
+
+/* Void spawn */
+UPDATE creature_template SET AIName = "NullCreatureAI" where entry = 25824;
+
+/* Black hole */
+UPDATE creature_template SET AIName = "NullCreatureAI" where entry = 25855;
