96b57f90ee22750b048c321ee25b8f99c7261c7b
SUN-Leekie <mathman.pro@gmail.com>
2012-06-06 20:52:20 +0200

    Script/Razorfen Downs: Add hook 'OnKillCreature', fix bugs
    Former-commit-id: e725c9480de67aa97db5f23afdadcaf8d7d41d69

---
+++ sql/updates/4457_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 7351;
+/* Tomb Reaver - Web */
+INSERT INTO `smart_scripts` (`entryorguid`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `target_type`, `comment`) VALUES (7351, 0, 5000, 8000, 7000, 16000, 11, 745, 2, 'Tomb Reaver - Web');
