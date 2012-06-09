44e3bc4d0fb4aa550e92a11c7a92cf588d3ecd7e
DrThum <DrThum@users.noreply.github.com>
2012-06-09 23:18:23 +0200

    Buru: Fixed typos
    Former-commit-id: 433e03f1e773c418bcab776c1dd21f0cc6b665b9

--- sql/updates/4474_world.sql
+++ sql/updates/4474_world.sql
@@ -1,14 +1,14 @@
 /* Buru the Gorger */
 DELETE FROM creature_scripts WHERE entryorguid = 15370;
-INSERT INTO creature_scripts (15370, 'boss_buru');
+INSERT INTO creature_scripts VALUES (15370, 'boss_buru');
 
 /* Buru Egg */
 DELETE FROM creature_scripts WHERE entryorguid = 15514;
-INSERT INTO creature_scripts (15514, 'buru_egg');
+INSERT INTO creature_scripts VALUES (15514, 'buru_egg');
 
 /* Buru Egg Trigger*/
 DELETE FROM creature_scripts WHERE entryorguid = 15964;
-INSERT INTO creature_scripts (15964, 'buru_egg_trigger');
+INSERT INTO creature_scripts VALUES (15964, 'buru_egg_trigger');
 
 /* Buru Egg Spawn */
 UPDATE creature SET id = 15514, spawntimesecs = 30 WHERE id = 15521;
