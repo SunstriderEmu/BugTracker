9eb95f7b41d24eeb81c9e277578dc6147a47a1ee
DrThum <DrThum@users.noreply.github.com>
2012-06-16 22:28:02 +0200

    Commented out a change for now, will be applied on June 24th
    Former-commit-id: 86eefdc9a3683899f34bf16b4e081162ce3b8426

--- sql/updates/4503_world.sql
+++ sql/updates/4503_world.sql
@@ -2,10 +2,10 @@
 DELETE FROM quest_pool WHERE pool_id = 2;
 INSERT INTO quest_pool VALUES (2, 11339), (2, 11340), (2, 11341), (2, 11342);
 INSERT INTO quest_pool_current VALUES (2, 11339);
-UPDATE creature_template SET pool_id = 2 WHERE entry = 15350;
+-- UPDATE creature_template SET pool_id = 2 WHERE entry = 15350;
 
 -- Alliance
 DELETE FROM quest_pool WHERE pool_id = 3;
 INSERT INTO quest_pool VALUES (3, 11335), (3, 11336), (3, 11337), (3, 11338);
 INSERT INTO quest_pool_current VALUES (3, 11335);
-UPDATE creature_template SET pool_id = 3 WHERE entry = 15351;
+-- UPDATE creature_template SET pool_id = 3 WHERE entry = 15351;
