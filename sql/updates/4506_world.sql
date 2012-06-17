3a91f6dbdd8ae0a45a1b42c4aebb2e4e7babdac2
DrThum <DrThum@users.noreply.github.com>
2012-06-17 11:24:37 +0200

    Resynchronised BG holidays starting dates
    Former-commit-id: 102a5168b01ea7e0270ad8c3767c88e7e5af7fce

---
+++ sql/updates/4506_world.sql
@@ -0,0 +1,4 @@
+UPDATE game_event SET start_time = FROM_UNIXTIME(1340920800) WHERE entry = 18;
+UPDATE game_event SET start_time = FROM_UNIXTIME(1341525600) WHERE entry = 19;
+UPDATE game_event SET start_time = FROM_UNIXTIME(1342130400) WHERE entry = 20;
+UPDATE game_event SET start_time = FROM_UNIXTIME(1342735200) WHERE entry = 21;
