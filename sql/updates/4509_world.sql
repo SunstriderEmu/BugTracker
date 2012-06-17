4931d51df15e7dfa907c9ddb71821ae9e505a999
DrThum <DrThum@users.noreply.github.com>
2012-06-17 14:08:18 +0200

    Fixed all herodays reputation gains
    Former-commit-id: 28f3a5c9cb43bb78a92b94aef5fdacd606dc1dce

---
+++ sql/updates/4509_world.sql
@@ -0,0 +1,12 @@
+UPDATE quest_template SET RewRepFaction1 = 933, RewRepValue1 = 350, RewRepFaction2 = 942, RewRepValue2 = 350 WHERE entry = 11369;
+UPDATE quest_template SET RewRepFaction1 = 933, RewRepValue1 = 350, RewRepFaction2 = 989, RewRepValue2 = 350 WHERE entry = 11382;
+UPDATE quest_template SET RewRepFaction1 = 933, RewRepValue1 = 350, RewRepFaction2 = 935, RewRepValue2 = 350 WHERE entry = 11384;
+UPDATE quest_template SET RewRepFaction1 = 933, RewRepValue1 = 350, RewRepFaction2 = 1011, RewRepValue2 = 350 WHERE entry = 11375;
+UPDATE quest_template SET RewRepFaction1 = 933, RewRepValue1 = 350, RewRepFaction2 = 935, RewRepValue2 = 350 WHERE entry = 11386;
+UPDATE quest_template SET RewRepFaction1 = 933, RewRepValue1 = 500, RewRepFaction2 = 0, RewRepValue2 = 0 WHERE entry = 11373;
+UPDATE quest_template SET RewRepFaction1 = 933, RewRepValue1 = 350, RewRepFaction2 = 989, RewRepValue2 = 350 WHERE entry = 11378;
+UPDATE quest_template SET RewRepFaction1 = 933, RewRepValue1 = 350, RewRepFaction2 = 1011, RewRepValue2 = 350 WHERE entry = 11374;
+UPDATE quest_template SET RewRepFaction1 = 933, RewRepValue1 = 350, RewRepFaction2 = 1011, RewRepValue2 = 350 WHERE entry = 11372;
+UPDATE quest_template SET RewRepFaction1 = 933, RewRepValue1 = 350, RewRepFaction2 = 942, RewRepValue2 = 350 WHERE entry = 11368;
+UPDATE quest_template SET RewRepFaction1 = 933, RewRepValue1 = 350, RewRepFaction2 = 935, RewRepValue2 = 350 WHERE entry = 11388;
+UPDATE quest_template SET RewRepFaction1 = 933, RewRepValue1 = 350, RewRepFaction2 = 942, RewRepValue2 = 350 WHERE entry = 11370;
