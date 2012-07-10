8dfc7e99882f735b5950b85cef5f0a79ef1215e1
SUN-Leekie <mathman.pro@gmail.com>
2012-07-10 19:10:36 +0200

    Script/M'uru: clean script and fix move for black hole
    Former-commit-id: 62ae0cff5567a9355b86ecd50b21447b8e205a5c

---
+++ sql/updates/4557_world.sql
@@ -0,0 +1,7 @@
+UPDATE creature_template SET AIName = "NullCreatureAI" WHERE entry = 25741;
+UPDATE creature_template SET AIName = "NullCreatureAI" WHERE entry = 25744;
+UPDATE creature_template SET AIName = "NullCreatureAI" WHERE entry = 25770;
+UPDATE creature_template SET AIName = "NullCreatureAI" WHERE entry = 25772;
+UPDATE creature_template SET AIName = "NullCreatureAI" WHERE entry = 25840;
+UPDATE creature_template SET AIName = "NullCreatureAI" WHERE entry = 25855;
+UPDATE creature_template SET AIName = "NullCreatureAI" WHERE entry = 25824;
