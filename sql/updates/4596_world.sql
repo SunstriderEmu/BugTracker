2f1561ec74ef5c6bd47caa4f01845ba404a63c44
SUN-Leekie <mathman.pro@gmail.com>
2012-09-09 21:10:46 +0200

    Script/Sunwell : Fix Sunblade Protector
    Former-commit-id: d355198d6df413a96633a3839d7f373cb61eed43

---
+++ sql/updates/4596_world.sql
@@ -0,0 +1,4 @@
+DELETE FROM `script_texts` WHERE `entry` IN (-1580115, -1580116, -1580117);
+INSERT INTO `script_texts` (`entry`, `content_default`, `content_loc2`, `type`) VALUES (-1580115, "Unité opérationnelle. Attaque des cibles en cours.", "Unité opérationnelle. Attaque des cibles en cours.", 1);
+INSERT INTO `script_texts` (`entry`, `content_default`, `content_loc2`, `type`) VALUES (-1580116, "Activation du mode d'économie d'énergie.", "Activation du mode d'économie d'énergie.", 1);
+INSERT INTO `script_texts` (`entry`, `content_default`, `content_loc2`, `type`) VALUES (-1580117, "Présence ennemie détectée.", "Présence ennemie détectée.", 1);
