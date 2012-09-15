8d964c36dce61efb3e9f72a98b5df6435b51cb34
DrThum <DrThum@users.noreply.github.com>
2012-09-15 09:43:45 +0200

    In case of almost full mailbox, only send the 100 first mails to prevent client crash, and send a notification to the player
    Former-commit-id: da99031e437484fda1cac7b1e4c4ecbf6608e3c4

---
+++ sql/updates/4604_world.sql
@@ -0,0 +1,2 @@
+DELETE FROM trinity_string WHERE entry = 20;
+INSERT INTO trinity_string (entry, content_default, content_loc2) VALUES (20, "Your mailbox is almost full. We sent you the 100 first mails to prevent a client crash.", "Votre boîte aux lettres est presque pleine. Nous vous avons envoyé seulement les 100 premiers courriers pour éviter un crash de votre client de jeu.");
