05ae55c0a7a8fd917a33b26f3f7b85290c8ca87f
SUN-Leekie <mathman.pro@gmail.com>
2012-09-16 16:42:25 +0200

    Sql/Error: Fix sql error 4610_world.sql
    Former-commit-id: e84bd765ec9a102df1c58d8247aab3a71e85513a

--- sql/updates/4610_world.sql
+++ sql/updates/4610_world.sql
@@ -2,4 +2,4 @@ UPDATE `eventai_scripts` SET `action1_param2` = 0 WHERE `creature_id` = 25509 AN
 
 UPDATE `eventai_scripts` SET `event_param1` = 10000, `event_param2` = 15000, `event_param3` = 10000, `event_param4` = 15000, `action1_param3` = 1 WHERE `creature_id` = 25597 AND `id` = 2559703;
 UPDATE `eventai_scripts` SET `event_param1` = 3000, `event_param2` = 3000, `event_param3` = 3000, `event_param4` = 3000 WHERE `creature_id` = 25597 AND `id` = 2559702;
-INSERT INTO `eventai_scripts` (`id`, `creature_id`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`) VALUES (25597, 2559704, 3, 10000, 15000, 10000, 15000, 11, 43991, 4, 1);
+INSERT INTO `eventai_scripts` (`id`, `creature_id`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`) VALUES (2559704, 25597, 3, 10000, 15000, 10000, 15000, 11, 43991, 4, 1);
