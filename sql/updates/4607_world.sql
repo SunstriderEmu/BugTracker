662e12974fdfd26ab0c1eb420040c91f5cf156f9
SUN-Leekie <mathman.pro@gmail.com>
2012-09-16 12:52:32 +0200

    Script/sunwell : add imune of some trashs
    Former-commit-id: 3d1f3d8c3b3683439969645f4a2212f2c46b3cca

---
+++ sql/updates/4607_world.sql
@@ -0,0 +1,9 @@
+UPDATE `creature_template` SET `minhealth` = 432296, `maxhealth` = 432296 WHERE `entry` = 25599;
+UPDATE `creature_template` SET `mechanic_immune_mask` = 131089 WHERE `entry` = 25599;
+UPDATE `creature_template` SET `mechanic_immune_mask` = 133137 WHERE `entry` = 25595;
+UPDATE `creature_template` SET `mechanic_immune_mask` = 132177 WHERE `entry` = 25592;
+UPDATE `creature_template` SET `mechanic_immune_mask` = 131089 WHERE `entry` = 25591;
+UPDATE `creature_template` SET `mechanic_immune_mask` = 132177 WHERE `entry` = 25948;
+UPDATE `creature_template` SET `mechanic_immune_mask` = 131089 WHERE `entry` = 25593;
+UPDATE `creature_template` SET `mechanic_immune_mask` = 131089 WHERE `entry` = 25597;
+UPDATE `creature_template` SET `mechanic_immune_mask` = 131089 WHERE `entry` = 25509;
