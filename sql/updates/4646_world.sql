064d7f71ecf50a4cec59d271a3e828bde794e796
DrThum <DrThum@users.noreply.github.com>
2012-10-19 23:18:10 +0200

    Sunwell: Fixed teleport to Twins room
    Former-commit-id: 26522781ab360508db94ff2e61d8d0d873e0d87a

--- sql/updates/4646_world.sql
+++ sql/updates/4646_world.sql
@@ -1,4 +1,4 @@
 UPDATE creature_template SET mindmg = 6650, maxdmg = 20000 WHERE entry = 25599;
 UPDATE creature_template SET mindmg = 2500, maxdmg = 20000 WHERE entry = 25595;
-DELETE FROM spell_target_position WHERE id = 46879;
-INSERT INTO spell_target_position VALUES (46879, 580, 1823.103271, 644.404907, 33.403900, 1.220804);
+DELETE FROM spell_target_position WHERE id = 46883;
+INSERT INTO spell_target_position VALUES (46883, 580, 1823.103271, 644.404907, 33.403900, 1.220804);
