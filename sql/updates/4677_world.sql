c2e1767369b2f60b0638e725a3465e84110228e5
SUN-Leekie <mathman.pro@gmail.com>
2012-10-30 20:59:51 +0100

    Script/Sunwell : severals fix for trashs
    Former-commit-id: 20fb2fe5c64905dbdea38c333611d9788d51e670

---
+++ sql/updates/4677_world.sql
@@ -0,0 +1,31 @@
+/* Damage for Sunblade Protector */
+UPDATE `creature_template` SET 
+    `mindmg` = 25000, 
+    `maxdmg` = 30000, 
+    `attackpower` = ROUND((`mindmg` + `maxdmg`) / 4 * 7), 
+    `mindmg` = ROUND(`mindmg` - `attackpower` / 7), 
+    `maxdmg` = ROUND(`maxdmg` - `attackpower` / 7) 
+  WHERE `entry` = 25507;
+
+/* Script for Fire Fiend */
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 26101;
+INSERT INTO `smart_scripts` (`entryorguid`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`) VALUES (26101, 6, 5000, 10000, 5000, 10000, 11, 46551);
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 26101;
+
+/* Damage for Sunblade Vindicator */
+UPDATE `creature_template` SET 
+    `mindmg` = 25000, 
+    `maxdmg` = 30000, 
+    `attackpower` = ROUND((`mindmg` + `maxdmg`) / 4 * 7), 
+    `mindmg` = ROUND(`mindmg` - `attackpower` / 7), 
+    `maxdmg` = ROUND(`maxdmg` - `attackpower` / 7) 
+  WHERE `entry` = 25369;
+
+/* Damage for  Kalecgos */
+UPDATE `creature_template` SET 
+    `mindmg` = 25000, 
+    `maxdmg` = 30000, 
+    `attackpower` = ROUND((`mindmg` + `maxdmg`) / 4 * 7), 
+    `mindmg` = ROUND(`mindmg` - `attackpower` / 7), 
+    `maxdmg` = ROUND(`maxdmg` - `attackpower` / 7) 
+  WHERE `entry` = 24850;
