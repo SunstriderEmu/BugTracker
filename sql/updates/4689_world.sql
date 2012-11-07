3d4fa2f2aaa8de8c2c58b7f5e20d1bc00dc25240
DrThum <DrThum@users.noreply.github.com>
2012-11-07 20:26:14 +0100

    Archimonde: New Doomfire implementation by Selenale
    Former-commit-id: b296b9893976512b074183f5161f82021304a5ab

--- sql/updates/4689_world.sql
+++ sql/updates/4689_world.sql
@@ -1,1 +1,4 @@
 UPDATE spell_affect SET SpellFamilyMask = SpellFamilyMask | 0x800000 WHERE entry BETWEEN 20101 AND 20105;
+
+DELETE FROM creature_scripts WHERE entryorguid = 18104;
+INSERT INTO creature_scripts VALUES (18095, "mob_doomfire"), (18104, "mob_doomfire_targeting");
---
+++ sql/updates/4695_world.sql
@@ -0,0 +1,1 @@
+UPDATE creature_template SET faction_a = 31, faction_h = 31, scale = 2 WHERE entry IN (18095, 18104);
