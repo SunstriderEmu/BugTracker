702d642ddbd9e575a7f2475c1cc3da2a3a9f47b6
DrThum <DrThum@users.noreply.github.com>
2012-11-07 20:49:07 +0100

    Forgotten SQL patch for archimonde doomfire
    Former-commit-id: 5c004d69e6b37cb70b1494cf00b06720119ea3c4

--- sql/updates/4695_world.sql
+++ sql/updates/4695_world.sql
@@ -1,1 +1,4 @@
 UPDATE creature_template SET faction_a = 31, faction_h = 31, scale = 2 WHERE entry IN (18095, 18104);
+
+DELETE FROM creature_scripts WHERE entryorguid = 18104;
+INSERT INTO creature_scripts VALUES (18095, "mob_doomfire"), (18104, "mob_doomfire_targeting");
