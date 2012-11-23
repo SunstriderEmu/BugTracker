9a29a90840dbe21b0331025d1c6ca168407f2de8
DrThum <DrThum@users.noreply.github.com>
2012-11-23 16:53:54 +0100

    Fixed SQL patch from previous commit
    Former-commit-id: 98c3a0fb14434555ba60d3619bc4f2eb6d648dfa

--- sql/updates/4746_world.sql
+++ sql/updates/4746_world.sql
@@ -1,4 +1,4 @@
 UPDATE `creature_template` SET `speed` = 2 WHERE `entry` = 17946;
 UPDATE `creature_template` SET `scale` = 0.6 WHERE `entry` = 17968;
 UPDATE `creature` SET `position_x` = 5600, `position_y` = -3462, `position_z` = 1577.1 WHERE `id` = 17968;
-UPDATE `creature_template` SET `spell1` = 0, `spell2` = 0, SET `unit_flags` = 32832 WHERE `entry` = 21865;
+UPDATE `creature_template` SET `spell1` = 0, `spell2` = 0, `unit_flags` = 32832 WHERE `entry` = 21865;
