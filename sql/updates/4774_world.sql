6bf201c22bef19893abf7e5b19faab8bf962a8e3
DrThum <DrThum@users.noreply.github.com>
2012-11-25 19:27:09 +0100

    Added npc 14474 by Selenale
    Former-commit-id: d7a5fa21c7128b4baf585357f3e00a9b531a168b

---
+++ sql/updates/4774_world.sql
@@ -0,0 +1,2 @@
+INSERT INTO `creature` (`id`, `map`, `spawnMask`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`) VALUES ('14494','0','1','3328.14','-3019.34','171.282','5.60083','900');
+INSERT INTO `creature_template_addon` (`entry`, `auras`) VALUES ('14494','17622 0');
