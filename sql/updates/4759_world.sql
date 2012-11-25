425835bf493f96c9e0a31fbf5cd71b72c22be5b9
SUN-Leekie <mathman.pro@gmail.com>
2012-11-25 15:12:19 +0100

    Script/KilJaeden : rescript boss
    Former-commit-id: 0bbc61328797408eb4b61225e8d1df6225bd384d

---
+++ sql/updates/4759_world.sql
@@ -0,0 +1,18 @@
+UPDATE creature_template SET AIName = '', ScriptName = '' WHERE entry IN (25319, 25315, 25608, 25588, 25603, 25598, 25735, 25502, 25708);
+
+DELETE FROM creature_scripts WHERE entryorguid IN (25319, 25315, 25608, 25588, 25603, 25598, 25735, 25502, 25708);
+INSERT INTO creature_scripts (25319, 'boss_kalecgos_kj');
+INSERT INTO creature_scripts (25315, 'boss_kiljaeden');
+INSERT INTO creature_scripts (25608, 'mob_kiljaeden_controller');
+INSERT INTO creature_scripts (25588, 'mob_hand_of_the_deceiver');
+INSERT INTO creature_scripts (25603, 'mob_felfire_portal');
+INSERT INTO creature_scripts (25598, 'mob_volatile_felfire_fiend');
+INSERT INTO creature_scripts (25735, 'mob_armageddon');
+INSERT INTO creature_scripts (25502, 'mob_shield_orb');
+INSERT INTO creature_scripts (25708, 'mob_sinster_reflection');
+
+/* Hand of the Deceiver equip*/
+DELETE FROM `creature_equip_template` WHERE entry = 3004;
+INSERT INTO `creature_equip_template` (`entry`, `equipmodel1`) VALUES (3004, 47994);
+
+UPDATE creature_template SET minhealth = 206000, maxhealth = 206000, minmana = 165450, maxmana = 165450,  mechanic_immune_mask = 65616, speed = 1.71, equipment_id = 3004 WHERE entry = 25588;
