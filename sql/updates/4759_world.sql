2fdd2fc4165452c1a9cb27f4be24c2d9e78108e1
DrThum <DrThum@users.noreply.github.com>
2012-11-25 16:29:26 +0100

    Fixed SQL patch
    Former-commit-id: 8d10210aa39db955975ed399eedfe67c277aecaa

--- sql/updates/4759_world.sql
+++ sql/updates/4759_world.sql
@@ -1,15 +1,15 @@
 UPDATE creature_template SET AIName = '', ScriptName = '' WHERE entry IN (25319, 25315, 25608, 25588, 25603, 25598, 25735, 25502, 25708);
 
 DELETE FROM creature_scripts WHERE entryorguid IN (25319, 25315, 25608, 25588, 25603, 25598, 25735, 25502, 25708);
-INSERT INTO creature_scripts (25319, 'boss_kalecgos_kj');
-INSERT INTO creature_scripts (25315, 'boss_kiljaeden');
-INSERT INTO creature_scripts (25608, 'mob_kiljaeden_controller');
-INSERT INTO creature_scripts (25588, 'mob_hand_of_the_deceiver');
-INSERT INTO creature_scripts (25603, 'mob_felfire_portal');
-INSERT INTO creature_scripts (25598, 'mob_volatile_felfire_fiend');
-INSERT INTO creature_scripts (25735, 'mob_armageddon');
-INSERT INTO creature_scripts (25502, 'mob_shield_orb');
-INSERT INTO creature_scripts (25708, 'mob_sinster_reflection');
+INSERT INTO creature_scripts VALUES (25319, 'boss_kalecgos_kj');
+INSERT INTO creature_scripts VALUES (25315, 'boss_kiljaeden');
+INSERT INTO creature_scripts VALUES (25608, 'mob_kiljaeden_controller');
+INSERT INTO creature_scripts VALUES (25588, 'mob_hand_of_the_deceiver');
+INSERT INTO creature_scripts VALUES (25603, 'mob_felfire_portal');
+INSERT INTO creature_scripts VALUES (25598, 'mob_volatile_felfire_fiend');
+INSERT INTO creature_scripts VALUES (25735, 'mob_armageddon');
+INSERT INTO creature_scripts VALUES (25502, 'mob_shield_orb');
+INSERT INTO creature_scripts VALUES (25708, 'mob_sinster_reflection');
 
 /* Hand of the Deceiver equip*/
 DELETE FROM `creature_equip_template` WHERE entry = 3004;
