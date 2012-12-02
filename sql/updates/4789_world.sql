3a434db11a44b1c3f5535e49e875f7df1663ce5f
DrThum <DrThum@users.noreply.github.com>
2012-12-02 11:04:29 +0100

    Added loot for Warpwood Pods in Dire Maul - by Kelno
    Former-commit-id: 1e2b3374c006c648fb3b4c5e727595e511d4d483

---
+++ sql/updates/4789_world.sql
@@ -0,0 +1,9 @@
+UPDATE gameobject_template SET data1 = entry WHERE entry IN (179526, 179528, 179532, 179533);
+INSERT INTO gameobject_loot_template VALUES ('179526','18297','114', '0', '1', '2', '0', '0', '0');
+INSERT INTO gameobject_loot_template VALUES ('179526','18255','57', '0', '1', '1', '0', '0', '0');
+INSERT INTO gameobject_loot_template VALUES ('179528','18297','114', '0', '1', '2', '0', '0', '0');
+INSERT INTO gameobject_loot_template VALUES ('179528','18255','57', '0', '1', '1', '0', '0', '0');
+INSERT INTO gameobject_loot_template VALUES ('179532','18297','114', '0', '1', '2', '0', '0', '0');
+INSERT INTO gameobject_loot_template VALUES ('179532','18255','57', '0', '1', '1', '0', '0', '0');
+INSERT INTO gameobject_loot_template VALUES ('179533','18297','114', '0', '1', '2', '0', '0', '0');
+INSERT INTO gameobject_loot_template VALUES ('179533','18255','57', '0', '1', '1', '0', '0', '0');
