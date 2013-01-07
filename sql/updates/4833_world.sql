d81a9a29d6686e2d3498c8d794fd7d13b5156ff6
SUN-Leekie <mathman.pro@gmail.com>
2013-01-07 21:36:46 +0100

    Script/KilJaeden : Fix damage on Sinister Reflection
    Former-commit-id: 1f5d304685edb4a7e45643d2a24f21b1c41aaf76

--- sql/updates/4833_world.sql
+++ sql/updates/4833_world.sql
@@ -1,1 +1,9 @@
 UPDATE creature_template SET scale = 1.5, minlevel = 70, maxlevel = 70, minhealth = 110000, maxhealth = 110000, minmana = 78875, maxmana = 78875, rank = 1, mechanic_immune_mask = 68944 WHERE entry = 25708;
+
+UPDATE `creature_template` SET 
+    `mindmg` = 8000,
+    `maxdmg` = 13000,
+    `attackpower` = ROUND((`mindmg` + `maxdmg`) / 4 * 7),
+    `mindmg` = ROUND(`mindmg` - `attackpower` / 7),
+    `maxdmg` = ROUND(`maxdmg` - `attackpower` / 7)
+  WHERE `entry` = 25708;
