de95ce0af000074b78cc0f5dbfc0095b6a82f35b
SUN-Leekie <mathman.pro@gmail.com>
2013-01-07 21:09:51 +0100

    Script/JilJaeden : fix issues on Sinister Reflection
    Former-commit-id: 1381c0b7f9c3035709c63db863ead8b35c890d1d

---
+++ sql/updates/4833_world.sql
@@ -0,0 +1,1 @@
+UPDATE creature_template SET scale = 1.5, minlevel = 70, maxlevel = 70, minhealth = 110000, maxhealth = 110000, minmana = 78875, maxmana = 78875, rank = 1, mechanic_immune_mask = 68944 WHERE entry = 25708;
