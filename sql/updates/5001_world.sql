fe5763048fe93c550d2a3b8be4abcf759955888e
SUN-Leekie <mathman.pro@gmail.com>
2013-04-15 21:02:10 +0200

    Script/Lurker Below : rewrite script
    Former-commit-id: ae02a5fe92a9d4eaaa6bead01330b22e63043014

---
+++ sql/updates/5001_world.sql
@@ -0,0 +1,10 @@
+UPDATE `creature_template` SET `AIName` = 'NullCreatureAI', `ScriptName` = '' WHERE `entry` IN (21217, 21873, 21865);
+
+DELETE FROM `creature_scripts` WHERE `entryorguid` = 21873;
+INSERT INTO `creature_scripts` VALUES (21873, 'Mob_Coilfang_Guardian');
+
+DELETE FROM `creature_scripts` WHERE `entryorguid` = 21865;
+INSERT INTO `creature_scripts` VALUES (21865, 'Mob_Coilfang_Ambusher');
+
+DELETE FROM `creature_scripts` WHERE `entryorguid` = 21217;
+INSERT INTO `creature_scripts` VALUES (21865, 'Boss_Lurker_Below');
