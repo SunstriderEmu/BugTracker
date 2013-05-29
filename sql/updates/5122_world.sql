4e8b888cdf84305e4cf3bc72f0e890f2114237c2
SUN-Leekie <mathman.pro@gmail.com>
2013-05-30 01:13:15 +0200

    Script/KilJaeden : change target for some spells
    Former-commit-id: b8d569659833dcdb4b6b1c35c3432dfb45192f67

--- sql/updates/5122_world.sql
+++ sql/updates/5122_world.sql
@@ -5,9 +5,3 @@ UPDATE `creature_template` SET
     `rangedattackpower` = 36750
   WHERE `entry` = 25708;
 
-/* Ranged damage for Coilfang Ambusher */
-UPDATE `creature_template` SET
-    `minrangedmg` = 3003,
-    `maxrangedmg` = 6139,
-    `rangedattackpower` = 31997
-  WHERE `entry` = 21873;
