2ec74722c620e2775343df64c51df3e80efd4684
SUN-Leekie <mathman.pro@gmail.com>
2013-05-30 00:15:09 +0200

    Script/Lurker : Fix shoot (thanks sele)
    Former-commit-id: 6a0031f9d0696f46ed4f8a2a481189aed051a92d

---
+++ sql/updates/5127_world.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_equip_template` SET `equipmodel1` = '2786', `equipmodel3` = '2786' WHERE `entry` = 3003;
+UPDATE `creature_template` SET `mindmg` = '930', `maxdmg` = '1898', `attackpower` = '9894', `minrangedmg` = '930', `maxrangedmg` = '1898', `rangedattackpower` = '9894' WHERE `entry` = 21865;
+
