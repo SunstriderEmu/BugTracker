d941f89a80404fae2a41b4fac9aec202de8603de
Kelno <kelnowm@gmail.com>
2013-06-07 23:41:49 +0200

    Changing default .blink distance
    Former-commit-id: 88e425bbbc8407a851459ee0e8ca82a497f59f26

--- sql/updates/5185_world.sql
+++ sql/updates/5185_world.sql
@@ -1,2 +1,2 @@
 delete from command where name = "blink";
-insert into command (name, security, help) values ("blink",2,"Teleport you a few meters ahead (5 by default).\n\nSyntax: .blink [#distance]");
+insert into command (name, security, help) values ("blink",2,"Teleport you a few meters ahead (15 by default).\n\nSyntax: .blink [#distance]");
