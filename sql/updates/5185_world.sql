d2f6e7b7f50d4400a6f17a364af74d6f62e388e8
DrThum <DrThum@users.noreply.github.com>
2013-06-09 12:15:35 +0200

    Fixed garbage in 5185_world.sql after merge
    Former-commit-id: 92ccd38a68947663fb6429137c1135d9eef46d59

--- sql/updates/5185_world.sql
+++ sql/updates/5185_world.sql
@@ -1,2 +1,2 @@
 delete from command where name = "blink";
-insert into command (name, security, help) values ("blink",2,"Teleport you a few meters ahead (15 by default).\n\nSyntax: .blink [#distance]");>>>>>>> other
+insert into command (name, security, help) values ("blink",2,"Teleport you a few meters ahead (15 by default).\n\nSyntax: .blink [#distance]");
