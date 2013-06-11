a61b95b32434b592421e76c7f86f258c4f2c62bd
SUN-Leekie <mathman.pro@gmail.com>
2013-06-11 22:13:26 +0200

    Script/KilJaeden : Fix call entropius on outro Script/Lurker : Fix ranged damage
    Former-commit-id: 4f366c090839932b42ad5813cab5937303b63a57

---
+++ sql/updates/5230_world.sql
@@ -0,0 +1,6 @@
+UPDATE `spell_template` SET `effect1` = 6 WHERE `id` = 46818;
+DELETE FROM `spell_script_target` WHERE `entry` = 46818;
+INSERT INTO (`entry`, `type`, `targetEntry`) VALUES (46818, 1, 26262);
+
+UPDATE `creature_template` SET `mindmg` = 930, `maxdmg` = 1898, `attackpower` = 9894, `minrangedmg` = 930, `maxrangedmg` = 1898, `rangedattackpower` = 5000 WHERE entry = 21865;
+
