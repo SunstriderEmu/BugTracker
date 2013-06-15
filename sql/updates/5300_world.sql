59926794e1b505b5dc7432dea6ce089cc22ffe36
Kelno <kelnowm@gmail.com>
2013-06-15 20:22:57 +0200

    Fixed some random npc dialog
    Former-commit-id: adef8eeb0d743ed59c458de3e150581b56a758b3

---
+++ sql/updates/5300_world.sql
@@ -0,0 +1,2 @@
+UPDATE db_script_string SET content_default = 'I\'m up.', content_loc2 = 'Je suis partant.' WHERE entry = 2000005046;
+UPDATE db_script_string SET content_loc2 = 'On essayerait de récupérer la prime de cet avis de recherche une fois notre garde terminée?' WHERE entry = 2000005047;
