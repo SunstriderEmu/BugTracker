a9c162675900a9f801d56c293c6ea1880431b964
Kelno <kelnowm@gmail.com>
2013-06-15 19:01:54 +0200

    Fixed Gatewatcher Iron-Hand(19710) jackhammer attack
    Former-commit-id: 2a8591b19861164fc167f8b7e3bb21707b6e8a5c

---
+++ sql/updates/5293_world.sql
@@ -0,0 +1,1 @@
+UPDATE script_texts SET content_default = 'Gatewatcher Iron-Hand raises his hammer menacingly...', content_loc2 = 'Gardien de porte Main-en-Fer lève son marteau d\'un air menaçant...' WHERE entry = -1554012;
