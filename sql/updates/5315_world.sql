5274c0be47907c9ad1b5cb4257abe5d157a20e1c
Kelno <kelnowm@gmail.com>
2013-06-17 02:03:01 +0200

    Various fixes on Ragnaros and Domo + hack to enable lava in MC
    Former-commit-id: e3c931fc56639b0d25f9d09734d9bb08e76b0f32

---
+++ sql/updates/5315_world.sql
@@ -0,0 +1,5 @@
+/* Added some immunities to Ragnaros sons */
+UPDATE creature_template SET mechanic_immune_mask = 79879 WHERE entry = 12143;
+/* Corrected Hammer of Ragnaros */
+DELETE FROM `eventai_scripts` where creature_id = 13148;
+insert into `eventai_scripts` (`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `action2_type`, `action2_param1`, `action2_param2`, `action2_param3`, `action3_type`, `action3_param1`, `action3_param2`, `action3_param3`, `comment`) values('2835502','13148','11','0','100','0','0','0','0','0','11','21155','0','2','17','46','33554434','0','20','0','0','0','Flame of Ragnaros - cast Intense Heat at spawn + not attackable + not selectable + no auto attack');
