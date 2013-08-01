7442e216a830a8f87e37b30f18b3063383366391
DrThum <DrThum@users.noreply.github.com>
2013-08-01 22:21:56 +0200

    Faction change : Send converted item by mail when it cannot be stored in the player's inventory
    Former-commit-id: 08d31ac347e26aa225e0b0bf3df04220fafac457

---
+++ sql/updates/5491_world.sql
@@ -0,0 +1,2 @@
+DELETE FROM player_factionchange_items WHERE alliance_id = 29471 AND horde_id = 29466;
+INSERT INTO player_factionchange_items VALUES (29471, "Renes de tigre de guerre noir", 29466, "Kodo de guerre noir");
