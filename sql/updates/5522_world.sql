546b238cecc95d114d45c3cc19fb4d6fd9908178
DrThum <DrThum@users.noreply.github.com>
2013-08-03 16:36:58 +0200

    Faction change: Added missing items
    Former-commit-id: ec704d8dbd94ba09d10813442487b71befc6aee1

---
+++ sql/updates/5522_world.sql
@@ -0,0 +1,5 @@
+INSERT INTO player_factionchange_items (alliance_id, horde_id) VALUES (2933, 6414), (2041, 0), (6087, 0), (2042, 0), (7003, 0), (5614, 0), (5815, 0), (0, 16987), (0, 3761), (0, 6504), (0, 6505), (0, 2231), (0, 4534), (0, 3822);
+
+DELETE FROM player_factionchange_items WHERE alliance_id IN (18863, 18858, 18862, 18864, 18857, 18854, 18859, 29593, 18856);
+INSERT INTO player_factionchange_items (alliance_id, horde_id) VALUES (18863, 18853), (18858, 18852), (18862, 18851), (18864, 29592), (18857, 18849), (18854, 18834), (18859, 18850), (29593, 18845), (18856, 18846);
+
