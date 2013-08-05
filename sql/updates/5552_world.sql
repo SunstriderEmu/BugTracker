28fe30a368ddfc6cf7d01ab9b75d27e6172dbdeb
DrThum <DrThum@users.noreply.github.com>
2013-08-05 20:24:44 +0200

    Faction change: Added some forgotten tabards (and added the classic auto-correction algorithm, to be removed in 2 weeks)
    Former-commit-id: 3aa2157fb4efd99bd934c05a71b141620c15008e

---
+++ sql/updates/5552_world.sql
@@ -0,0 +1,4 @@
+INSERT INTO player_factionchange_items (alliance_id, horde_id) VALUES (20132, 20131);
+INSERT INTO player_factionchange_items (alliance_id, horde_id) VALUES (19506, 19505);
+INSERT INTO player_factionchange_items (alliance_id, horde_id) VALUES (19032, 19031);
+
