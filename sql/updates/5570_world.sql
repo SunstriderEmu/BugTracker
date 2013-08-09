ec24127dcc01ffa3e0f9bbb8143b2efb59768296
DrThum <DrThum@users.noreply.github.com>
2013-08-09 19:38:19 +0200

    Added some more data to faction change - Kelno, don't forget to remove this in 1-2 weeks
    Former-commit-id: 2796fc12d33d8b6b4a1d26a98793c566ba144f17

---
+++ sql/updates/5570_world.sql
@@ -0,0 +1,2 @@
+DELETE FROM player_factionchange_items WHERE alliance_id IN (19030, 29465, 29467, 29468, 29471, 35906);
+INSERT INTO player_factionchange_items (alliance_id, horde_id) VALUES (19030, 19029), (29465, 29466), (29467, 29469), (29468, 29470), (29471, 29472), (35906, 34129), (19045, 19046);
