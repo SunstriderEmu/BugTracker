0f51586b96e58a05e1b1950e52464bf13b8a6774
Kelno <kelnowm@gmail.com>
2013-10-08 17:20:15 +0200

    Error in a sql few commits ago
    Former-commit-id: 8391d3e13e1790fee9d279ceee5b62bdca415dc9

--- sql/updates/5822_world.sql
+++ sql/updates/5822_world.sql
@@ -1,4 +1,4 @@
 /* Added no initial aggro for vampirich touch energize effect */
-UPDATE spell_template SET AttributesEx3 = 0x02020000 WHERE entry = 34919;
+UPDATE spell_template SET AttributesEx3 = 0x02020000 WHERE id = 34919;
 
 /* Vampiric embrace too? */
