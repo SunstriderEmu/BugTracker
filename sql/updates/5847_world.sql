b874d494d96f246e242001acec0f1c1efc4e6bb7
Kelno <kelnowm@gmail.com>
2013-10-11 19:02:02 +0200

    Added some missing equivalences for faction change
    Former-commit-id: 5ac9ed7d609e0f3e6abc3cf991798ff4d0fcb52a

---
+++ sql/updates/5847_world.sql
@@ -0,0 +1,4 @@
+/* Added some missing equivalence */
+REPLACE INTO player_factionchange_items VALUES (29745,"grand elekk bleu",28936,"faucon-pérégrin rose rapide");
+REPLACE INTO player_factionchange_items VALUES (29746,"grand elekk vert",29223,"faucon-pérégrin vert rapide");
+REPLACE INTO player_factionchange_items VALUES (29747,"grand elekk violet",29224,"faucon-pérégrin violet rapide");
