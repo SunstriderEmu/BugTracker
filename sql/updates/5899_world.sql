0350a71fab2c31455cf96ec4fbb783334612f996
Kelno <kelnowm@gmail.com>
2013-10-18 18:17:48 +0200

    UTF-8 joy
    Former-commit-id: 649e0ec22be0b84d88b717b01449c72e8022b0fc

--- sql/updates/5899_world.sql
+++ sql/updates/5899_world.sql
@@ -1,28 +1,28 @@
-/* Hyjal bosses texts */
+ï»¿/* Hyjal bosses texts */
 
 /* 1- Winterchill */
 REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
 VALUES (-1801000,"You have won this battle, but not... the... war.","Vous avez gagnez cette bataille, mais... pas la... guerre.",11026,1,"Rage Winterchill - on death");
 
 REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
-VALUES (-1801001,"All life must perish!","Toute vie doit périr !",11025,1,"Rage Winterchill - on slay 1");
+VALUES (-1801001,"All life must perish!","Toute vie doit pÃ©rir !",11025,1,"Rage Winterchill - on slay 1");
 REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
-VALUES (-1801002,"Victory to the Legion!","Victoire à la Légion !",11057,1,"Rage Winterchill - on slay 2");
+VALUES (-1801002,"Victory to the Legion!","Victoire Ã  la LÃ©gion !",11057,1,"Rage Winterchill - on slay 2");
 REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
-VALUES (-1801003,"Your world is ours now!","Votre monde est maintenant à nous !",11056,1,"Rage Winterchill - on slay 3");
+VALUES (-1801003,"Your world is ours now!","Votre monde est maintenant Ã  nous !",11056,1,"Rage Winterchill - on slay 3");
 
 REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
 VALUES (-1801004,"Crumble and rot!","Tombez et pourrissez !",11023,1,"Rage Winterchill - d&c 1");
 REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
-VALUES (-1801005,"Ashes to ashes, dust to dust.","Des cendres aux cendres, de poussière à poussière !",11055,1,"Rage Winterchill - d&c 2");
+VALUES (-1801005,"Ashes to ashes, dust to dust.","Des cendres aux cendres, de poussiÃ¨re Ã  poussiÃ¨re !",11055,1,"Rage Winterchill - d&c 2");
 
 REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
 VALUES (-1801006,"It will be much colder in your grave.","Il fera bien plus froid dans vos tombes.",11058,1,"Rage Winterchill - frost nova 1");
 REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
-VALUES (-1801007,"Succumb to the icy chill... of death!","Succombez au frisson glacé... de la mort.",11024,1,"Rage Winterchill - frost nova 2");
+VALUES (-1801007,"Succumb to the icy chill... of death!","Succombez au frisson glacÃ©... de la mort.",11024,1,"Rage Winterchill - frost nova 2");
 
 REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
-VALUES (-1801008,"The Legion's final conquest has begun! Once again the subjugation of this world is within our grasp. Let none survive!","L'ultime invasion de la Légion a commencé ! Une fois de plus, l'asservissement de ce monde est à notre portée. Ne faites pas de quartier !",11022,1,"Rage Winterchill - aggro");
+VALUES (-1801008,"The Legion's final conquest has begun! Once again the subjugation of this world is within our grasp. Let none survive!","L'ultime invasion de la LÃ©gion a commencÃ© ! Une fois de plus, l'asservissement de ce monde est Ã  notre portÃ©e. Ne faites pas de quartier !",11022,1,"Rage Winterchill - aggro");
 
 /* 2- Anatheron */
 
@@ -34,17 +34,17 @@ VALUES (-1801011,"Your hopes are lost!","Tout espoir, est perdu.",10981,1,"Anath
 REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
 VALUES (-1801012,"Scream for me!","Je veux vous entendre crier.",11038,1,"Anatheron - On slay 2");
 REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
-VALUES (-1801013,"Pity, no time for a slow death!","Dommage, pas le temps de vous faire mourir à petit feu.",11039,1,"Anatheron - On slay 3");
+VALUES (-1801013,"Pity, no time for a slow death!","Dommage, pas le temps de vous faire mourir Ã  petit feu.",11039,1,"Anatheron - On slay 3");
 
 REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
-VALUES (-1801014,"The swarm is eager to feed!","L'essaim est affamé.",10979,1,"Anatheron - Swarm 1");
+VALUES (-1801014,"The swarm is eager to feed!","L'essaim est affamÃ©,10979,1,"Anatheron - Swarm 1");
 REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
 VALUES (-1801015,"Pestilence upon you!","La peste soit sur vous !",11037,1,"Anatheron - Swarm 2");
 
 REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
-VALUES (-1801016,"You look tired...","Vous semblez fatigué...",10978,1,"Anatheron - Sleep 1");
+VALUES (-1801016,"You look tired...","Vous semblez fatiguÃ©.",10978,1,"Anatheron - Sleep 1");
 REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
-VALUES (-1801017,"Sweet dreams...","Faites de beaux rêves...",11545,1,"Anatheron - Sleep 2");
+VALUES (-1801017,"Sweet dreams...","Faites de beaux rÃªves...",11545,1,"Anatheron - Sleep 2");
 
 REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
 VALUES (-1801018,"Let fire rain from above!","Que le feu pleuve du ciel !",10980,1,"Anatheron - Inferno 1");
@@ -52,12 +52,12 @@ REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comm
 VALUES (-1801019,"Earth and sky shall burn!","Que la Terre et le Ciel s'embrasent !",11036,1,"Anatheron - Inferno 2");
 
 REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
-VALUES (-1801020,"You are defenders of a doomed world! Flee here, and perhaps you will prolong your pathetic lives!","Vous défendez un monde condamné. Fuyez si vous voulez avoir une chance de faire durer vos vies pathétiques.",10977,1,"Anatheron - On aggro");
+VALUES (-1801020,"You are defenders of a doomed world! Flee here, and perhaps you will prolong your pathetic lives!","Vous dÃ©fendez un monde condamnÃ©. Fuyez si vous voulez avoir une chance de faire durer vos vies pathÃ©tiques.",10977,1,"Anatheron - On aggro");
 
 /* 4- Azgalor */
 
 REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
-VALUES (-1801022,"Your time is almost... up","Votre temps viendra bientôt...",11002,1,"Azgalor - Death");
+VALUES (-1801022,"Your time is almost... up","Votre temps viendra bientÃ´t...",11002,1,"Azgalor - Death");
 
 REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
 VALUES (-1801023,"Reesh, hokta!","Reesh, hokta !",11001,1,"Azgalor - On slay 1");
@@ -67,26 +67,26 @@ REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comm
 VALUES (-1801025,"No one is going to save you.","Nul ne viendra vous sauver.",11047,1,"Azgalor - On slay 3");
 
 REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
-VALUES (-1801026,"Just a taste... of what awaits you.","Voilà juste un avant-goût... de ce qui vous attend.",11046,1,"Azgalor - Doom 1");
+VALUES (-1801026,"Just a taste... of what awaits you.","VoilÃ  juste un avant-goÃ»t... de ce qui vous attend.",11046,1,"Azgalor - Doom 1");
 REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
-VALUES (-1801027,"Suffer you despicable insect!","Souffre, méprisable insecte !",11000,1,"Azgalor - Doom 2");
+VALUES (-1801027,"Suffer you despicable insect!","Souffre, mÃ©prisable insecte !",11000,1,"Azgalor - Doom 2");
 
 REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
-VALUES (-1801028,"Abandon all hope! The legion has returned to finish what was begun so many years ago. This time there will be no escape!","Abandonnez tout espoir ! La Légion est revenue finir ce qui avait été commencé il y a bien longtemps. Cette fois, il n'y aura pas d'échappatoire !",10999,1,"Azgalor - On aggro");
+VALUES (-1801028,"Abandon all hope! The legion has returned to finish what was begun so many years ago. This time there will be no escape!","Abandonnez tout espoir ! La LÃ©gion est revenue finir ce qui avait Ã©tÃ© commencÃ© il y a bien longtemps. Cette fois, il n'y aura pas d'Ã©chappatoire !",10999,1,"Azgalor - On aggro");
 
 /* 3- Kaz'rogal */
 
 REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
 VALUES (-1801031, "Shaza-Kiel!","Shaza-kiel !",11017,1,"Kaz'rogal - On slay 1");
 REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
-VALUES (-1801032, "You... are nothing!","Vous... n'êtes rien !",11053,1,"Kaz'rogal - On slay 2");
+VALUES (-1801032, "You... are nothing!","Vous... n'Ãªtes rien !",11053,1,"Kaz'rogal - On slay 2");
 REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
-VALUES (-1801033, "Miserable nuisance!","Misérable nuisance !",11054,1,"Kaz'rogal - On slay 3");
+VALUES (-1801033, "Miserable nuisance!","MisÃ©rable nuisance !",11054,1,"Kaz'rogal - On slay 3");
 
 REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
 VALUES (-1801034, "Your death will be a painful one.","Vous mourrez dans d'atroces souffrances.",11016,1,"Kaz'rogal - Mark 1");
 REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
-VALUES (-1801035, "You... are marked.","Vous portez désormais... la marque.",11052,1,"Kaz'rogal - Mark 2");
+VALUES (-1801035, "You... are marked.","Vous portez dÃ©sormais... la marque.",11052,1,"Kaz'rogal - Mark 2");
 
 REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
-VALUES (-1801036, "Cry for mercy! Your meaningless lives will soon be forfeit.","Implorez grâce ! Vos vies insignifiantes seront bientôt balayées !",11015,1,"Kaz'rogal - Aggro");
+VALUES (-1801036, "Cry for mercy! Your meaningless lives will soon be forfeit.","Implorez grÃ¢ce ! Vos vies insignifiantes seront bientÃ´t balayÃ©es !",11015,1,"Kaz'rogal - Aggro");
