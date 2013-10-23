a180a7883b8e4aacd5a7bd0eed81d02115756516
Kelno <kelnowm@gmail.com>
2013-10-23 13:53:40 +0200

    Updated Hyjal texts (still missing french translations)
    Former-commit-id: 1e51b9546f6ece364d5ad86b2f47ec0863f3a3d9

---
+++ sql/updates/5924_world.sql
@@ -0,0 +1,53 @@
+
+/* Hyjal Gossip texts */
+
+REPLACE INTO npc_text(ID,text0_0) VALUES (395,"My companions and I are with you, Lady Proudmoore.");
+REPLACE INTO locales_npc_text(entry,Text0_0_loc2) VALUES (395,"Mes compagnons et moi sommes à vos côtés, dame Portvaillant."); 
+REPLACE INTO npc_text(ID,text0_0) VALUES (396,"We are ready for whatever Archimonde might send our way, Lady Proudmoore.");
+REPLACE INTO locales_npc_text(entry,Text0_0_loc2) VALUES (396,"X"); 
+
+REPLACE INTO npc_text(ID,text0_0) VALUES (397,"I am with you, Thrall.");
+REPLACE INTO locales_npc_text(entry,Text0_0_loc2) VALUES (397,"Je suis avec vous, Thrall."); 
+REPLACE INTO npc_text(ID,text0_0) VALUES (398,"We have nothing to fear.");
+REPLACE INTO locales_npc_text(entry,Text0_0_loc2) VALUES (398,"X"); 
+
+REPLACE INTO npc_text(ID,text0_0) VALUES (399,"Until we meet again, Lady Proudmoore.");
+REPLACE INTO locales_npc_text(entry,Text0_0_loc2) VALUES (399,"X"); 
+
+REPLACE INTO npc_text(ID,text0_0) VALUES (400,"I would be grateful for any aid you can provide, Priestess.");
+REPLACE INTO locales_npc_text(entry,Text0_0_loc2) VALUES (400,"X");
+
+REPLACE INTO npc_text(ID,text0_0) VALUES (402,"Until we meet again, Thrall.");
+REPLACE INTO locales_npc_text(entry,Text0_0_loc2) VALUES (402,"X");
+
+REPLACE INTO npc_text(ID,text0_0) VALUES (403,"I have no idea who you are, but your presence is most welcome. Archimonde and his Scourge army could attack at any time, $c. Will you stand with us against evil?");
+REPLACE INTO locales_npc_text(entry,Text0_0_loc2) VALUES (403,"Je ne sais pas du tout qui vous êtes, mais vous êtes l$ge:a; bienvenu$g:e;. Archimonde et son armée du Fléau peuvent attaquer à n'importe quel moment, $C. Combattrez-vous ce mal à nos côtés ?");
+REPLACE INTO npc_text(ID,text0_0) VALUES (404,"Thank you, $C. Now prepare yourselves. Archimonde is on the march and we must hold off the inevitable for as long as we can.");
+REPLACE INTO locales_npc_text(entry,Text0_0_loc2) VALUES (404,"Merci $C. Maintenant, préparez-vous, Archimonde est en marche, et nous devons retarder l'inévitable le plus longtemps possible.");
+REPLACE INTO npc_text(ID,text0_0) VALUES (405,"Well done, adventurer. Rage Winterchill's death has earned us all a small pause in the enemy's advance. Use this short time wisely. Tend to your wounded and prepare your defenses, as soon Archimonde will send even deadlier forces at us. My meager force of reserve units will reinforce you when battle is joined again.");
+REPLACE INTO locales_npc_text(entry,Text0_0_loc2) VALUES (405,"X");
+REPLACE INTO npc_text(ID,text0_0) VALUES (406,"Then let Archimonde do his worst.");
+REPLACE INTO locales_npc_text(entry,Text0_0_loc2) VALUES (406,"X");
+REPLACE INTO npc_text(ID,text0_0) VALUES (407,"Amazing! Anatheron's death at your hands has delayed Archimonde from his objective, and brought us that much closer to victory. My forces cannot stay here, however. My troops and I are exhausted and now is the time for retreat. If you strength has not waned, head east to Thrall's encampment. That is where we make another stand. Be well.");
+REPLACE INTO locales_npc_text(entry,Text0_0_loc2) VALUES (407,"X");
+
+REPLACE INTO npc_text(ID,text0_0) VALUES (408,"Lady Proudmoore's forces have bled to delay Archimonde and his Burning Legion, and now this grim task falls to us. Are you ready to stand with me against the onslaught?");
+REPLACE INTO locales_npc_text(entry,Text0_0_loc2) VALUES (408,"Les forces de dame Portvaillant se sont saignées à blanc poru retarder Archimonde et la Légion ardente. Cette terrible tâche nous revient. Vous tiendrez-vous à mes côtés pendant l'assaut ?");
+REPLACE INTO npc_text(ID,text0_0) VALUES (409,"Then let the Legion do his worst!");
+REPLACE INTO locales_npc_text(entry,Text0_0_loc2) VALUES (409,"Allors laissez la Légion se déchaîner !");
+REPLACE INTO npc_text(ID,text0_0) VALUES (410,"Outstanding! Kaz'rogal's death cry should strike fear into the core of the Burning Legion. They now know the power your forces possess, so expect the worst the Legion can offer.");
+REPLACE INTO locales_npc_text(entry,Text0_0_loc2) VALUES (410,"X");
+REPLACE INTO npc_text(ID,text0_0) VALUES (411,"May the spirits be with you.");
+REPLACE INTO locales_npc_text(entry,Text0_0_loc2) VALUES (411,"X");
+REPLACE INTO npc_text(ID,text0_0) VALUES (412,"Outstanding! Killing a Pit Lord such as Azgalor is no small feat, my friend. If we had more time, I would love to hear the story of you and your companions. Archimonde will not give us that pause, however. My scouts have already reported that the Legion is amassing an immense force outside our camp, and Archimonde is very close to the World Tree. We are evacuating with the help of Lady proudmoore. You should head to the World Tree at once. The fate of our world is still undecided.");
+REPLACE INTO locales_npc_text(entry,Text0_0_loc2) VALUES (412,"X");
+
+REPLACE INTO npc_text(ID,text0_0) VALUES (413,"The moment we've been dreading is now at hand, $C. Archimonde has made his way to Nordrassil, and is beginning to devour the World Tree's energies. Malfurion needs more time for his plan to work, so we must do the unthinkable. You must attack Archimonde directly, while my own troops hold off the streams of Burning Legion forces approaching the top of the mount.");
+REPLACE INTO locales_npc_text(entry,Text0_0_loc2) VALUES (413,"X");
+REPLACE INTO npc_text(ID,text0_0) VALUES (414,"The Teas of the Goddess have been blessed by Elune, and their power will help you combat Achimonde's vile magics. Use their power well.");
+REPLACE INTO locales_npc_text(entry,Text0_0_loc2) VALUES (414,"X");
+
+REPLACE INTO script_texts (entry,content_default,content_loc2, sound, type, comment)
+VALUES (-1543998,"All your efforts have been in vain, for the draining of the World Tree has already begun. Soon the heart of your world will beat no more!","X",0,1,"Archimonde - Yell at horde retreat");
+
+
