910df88eaa1836aedc9b4bcdba65092e4b01a964
Kelno <kelnowm@gmail.com>
2013-10-29 01:47:25 +0100

    Removed now unused column in spell_threat & fixed some moar spells threat
    Former-commit-id: 7cc55b47b7acbb5f0261ce4d0f8db3f617f67fc2

---
+++ sql/updates/5972_world.sql
@@ -0,0 +1,7 @@
+﻿#lacerate & devastate
+replace into `spell_threat` (`entry`, `flatMod`, `pctMod`) values('20243','76','0.5');
+replace into `spell_threat` (`entry`, `flatMod`, `pctMod`) values('30016','91','0.5');
+replace into `spell_threat` (`entry`, `flatMod`, `pctMod`) values('30022','106','0.5');
+replace into `spell_threat` (`entry`, `flatMod`, `pctMod`) values('33745','285','0.2');
+
+alter table spell_threat drop column apPctMod;
