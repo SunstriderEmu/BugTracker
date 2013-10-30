4f338cfd549af8cb89d27ad72b387a346679e0cb
Kelno <kelnowm@gmail.com>
2013-10-30 03:12:08 +0100

    Fixed lifebloom, devastate, switfmend & energize effects threat
    Former-commit-id: 262ee75b51eab2ddcc590e110abfd1f12929190f

---
+++ sql/updates/5976_world.sql
@@ -0,0 +1,8 @@
+﻿#Discretion talent (added lifebloom & swiftmend)
+UPDATE spell_affect
+SET SpellFamilyMask = 4509999143425010
+WHERE (entry <= 17122 AND entry >= 17118)
+AND effectId = 0;
+
+#Lifebloom threat
+REPLACE INTO spell_threat values (33763,0,0.5);
