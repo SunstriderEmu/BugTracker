9bfe01605ba9cab77e364242b035662892a0958c
Kelno <kelnowm@gmail.com>
2013-11-10 04:47:52 +0100

    Added Unit::HasAuraWithCaster and Unit::HasAuraWithCasterNot + Some more fixes on Shahraz + Fixed a problem with Unit::IsFriendlyTo (Damage AoE targeting ally was screwing it up)
    Former-commit-id: 5b214e4084bb7284953691eaa204918bbace32a6

---
+++ sql/updates/6018_world.sql
@@ -0,0 +1,11 @@
+DATE VERSION SET core_version = "Winrunner Rev: 6018";
+
+UPDATE spell_template
+SET effect2 = 6, effectImplicitTargetA2 = 1, effectApplyAuraName2 = 23, effectAmplitude2 = 15000
+WHERE id = 40879;
+
+#add dual wield to mother shahraz
+UPDATE creature_template SET flags_extra = 41 + 0x20 WHERE entry = 22947;
+
+#Fatal Attraction dummy aura set to 2 sec (this is used to determine if players are far away enough from each others)
+UPDATE spell_template SET durationIndex = 39 WHERE id = 40871;
