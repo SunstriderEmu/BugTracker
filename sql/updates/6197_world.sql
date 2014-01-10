67873d327593042e87c843a0f91d8afb5b7ff613
SUN-Leekie <mathman.pro@gmail.com>
2014-01-11 00:52:57 +0100

    Script/KJ : Move script
    Former-commit-id: 91c9d6f799149ac9a2e06b68465b2aa5e386afa5

---
+++ sql/updates/6197_world.sql
@@ -0,0 +1,5 @@
+DELETE FROM  `spell_linked_spell` WHERE `spell_trigger` = 45856;
+INSERT INTO `spell_linked_spell` VALUES (45856, 49725, 0, 'Breath: Haste');
+
+DELETE FROM `creature_scripts` WHERE `entryorguid` = 25653;
+
