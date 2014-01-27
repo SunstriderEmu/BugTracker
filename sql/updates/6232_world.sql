066b4c8941080d3f7e4050921a1500225c3d1eab
Kelno <kelnowm@gmail.com>
2014-01-27 14:46:43 +0100

    Fixed UTF-8 for some Illidan texts + more tweaks for intro
    Former-commit-id: 5c1b20cc8fd1a2122cdba54c1904e1a1bf623e42

--- sql/updates/6232_world.sql
+++ sql/updates/6232_world.sql
@@ -1,17 +1,17 @@
-REPLACE INTO script_texts (entry,content_default,content_loc2,sound,type,emote,comment) VALUES (-1566032,"Those who've defiled this temple have all been defeated. All but one !","Tous ceux qui avaient profané le temple ont été battus. Tous sauf un !",0,0,1,"SAY_AKAMA_INTRO1");
-REPLACE INTO script_texts (entry,content_default,content_loc2,sound,type,emote,comment) VALUES (-1566033,"Let us finish what we've started. I will lead you to Illidan's abode once you've recovered your strength.","Finissons ce que nous avons commencé. Je vous conduirai aux appartements d'Illidan dès que vous aurez repris vos forces.",0,0,1,"SAY_AKAMA_INTRO2");
-REPLACE INTO script_texts (entry,content_default,content_loc2,sound,type,emote,comment) VALUES (-1566034,"This door is all that stands between us and the Betrayer. Stand aside, friends.","Cette porte est tout ce qui reste entre nous et le Traître. Écartez-vous, mes amis.",0,0,1,"SAY_AKAMA_STANDASIDE");
+REPLACE INTO script_texts (entry,content_default,content_loc2,sound,type,emote,comment) VALUES (-1566032,"Those who've defiled this temple have all been defeated. All but one !","Tous ceux qui avaient profanÃ© le temple ont Ã©tÃ© battus. Tous sauf un !",0,0,1,"SAY_AKAMA_INTRO1");
+REPLACE INTO script_texts (entry,content_default,content_loc2,sound,type,emote,comment) VALUES (-1566033,"Let us finish what we've started. I will lead you to Illidan's abode once you've recovered your strength.","Finissons ce que nous avons commencÃ©. Je vous conduirai aux appartements d'Illidan dÃ¨s que vous aurez repris vos forces.",0,0,1,"SAY_AKAMA_INTRO2");
+REPLACE INTO script_texts (entry,content_default,content_loc2,sound,type,emote,comment) VALUES (-1566034,"This door is all that stands between us and the Betrayer. Stand aside, friends.","Cette porte est tout ce qui reste entre nous et le TraÃ®tre. Ã‰cartez-vous, mes amis.",0,0,1,"SAY_AKAMA_STANDASIDE");
 
 REPLACE INTO script_texts (entry,content_default,content_loc2,sound,type,emote,comment) VALUES (-1566035,"I cannot do this alone...","Je n'y arriverai pas seul...",0,0,1,"SAY_AKAMA_CHANNELFAIL");
-REPLACE INTO script_texts (entry,content_default,content_loc2,sound,type,emote,comment) VALUES (-1566036,"You are not alone, Akama.","Vous n'êtes pas seul, Akama.",0,0,1,"SAY_OLUM_NOTALONE");
+REPLACE INTO script_texts (entry,content_default,content_loc2,sound,type,emote,comment) VALUES (-1566036,"You are not alone, Akama.","Vous n'Ãªtes pas seul, Akama.",0,0,1,"SAY_OLUM_NOTALONE");
 REPLACE INTO script_texts (entry,content_default,content_loc2,sound,type,emote,comment) VALUES (-1566037,"You people will always be with you!","Votre peuple sera toujours avec vous !",0,0,1,"SAY_OLUM_NOTALONE");
-REPLACE INTO script_texts (entry,content_default,content_loc2,sound,type,emote,comment) VALUES (-1566038,"I thank you for your aid, brothers. Our people will be redeemed!","Merci pour votre aide, mes frères. Notre peuple aura sa rédemption !",0,0,1,"SAY_AKAMA_THANKS");
+REPLACE INTO script_texts (entry,content_default,content_loc2,sound,type,emote,comment) VALUES (-1566038,"I thank you for your aid, brothers. Our people will be redeemed!","Merci pour votre aide, mes frÃ¨res. Notre peuple aura sa rÃ©demption !",0,0,1,"SAY_AKAMA_THANKS");
 
 REPLACE INTO npc_text (ID,text0_0) VALUES (450,"I'm ready, Akama.");
-REPLACE INTO locales_npc_text (entry,Text0_0_loc2) VALUES (450,"Je suis prêt, Akama.");
+REPLACE INTO locales_npc_text (entry,Text0_0_loc2) VALUES (450,"Je suis prÃªt, Akama.");
 REPLACE INTO npc_text (ID,text0_0) VALUES (451,"The time has come to face Illidan, $N. Are you ready ?");
-REPLACE INTO locales_npc_text (entry,Text0_0_loc2) VALUES (451,"Il est temps d'affronter Illidan, $N. Êtes-vous prêt$g:e; ?");
-REPLACE INTO npc_text (ID,text0_0) VALUES (452,"We are ready to face Illidan");
-REPLACE INTO locales_npc_text (entry,Text0_0_loc2) VALUES (452,"Nous sommes prêts à affronter Illidan.");
+REPLACE INTO locales_npc_text (entry,Text0_0_loc2) VALUES (451,"Il est temps d'affronter Illidan, $N. ÃŠtes-vous prÃªt$g:e; ?");
+REPLACE INTO npc_text (ID,text0_0) VALUES (452,"We are ready to face Illidan.");
+REPLACE INTO locales_npc_text (entry,Text0_0_loc2) VALUES (452,"Nous sommes prÃªts Ã  affronter Illidan.");
 REPLACE INTO npc_text (ID,text0_0) VALUES (453,"Be wary, friends. The Betrayer meditates in the court just beyond.");
-REPLACE INTO locales_npc_text (entry,Text0_0_loc2) VALUES (453,"Prenez garde, mes amis. Le Traître ressasse ses pensées dans la cour devant nous.");
+REPLACE INTO locales_npc_text (entry,Text0_0_loc2) VALUES (453,"Prenez garde, mes amis. Le TraÃ®tre ressasse ses pensÃ©es dans la cour devant nous.");
