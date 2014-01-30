b5ee1ed2c5c4af60408dd2f06d3d2df5faa67e88
Kelno <kelnowm@gmail.com>
2014-01-30 18:17:57 +0100

    Fixed some characters in last commit
    Former-commit-id: 90cae53d0a5afda22dfb932287c2ac8597320624

--- sql/updates/6262_world.sql
+++ sql/updates/6262_world.sql
@@ -1,3 +1,3 @@
 update spell_template set effect3 = 0 where id IN (16998,16999);
 
-INSERT INTO spell_linked_spell VALUES (768,5225,0,"Track Humanoids on cat form");
+REPLACE INTO spell_linked_spell VALUES (768,5225,0,"Track Humanoids on cat form");
