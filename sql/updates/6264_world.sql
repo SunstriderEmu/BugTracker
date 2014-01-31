b350bfb11370172985abb53ed2c33a144a3f752d
Kelno <kelnowm@gmail.com>
2014-01-31 22:07:57 +0100

    Minor updates on Kil'Jaeden
    Former-commit-id: 7269cf66514e61dd1fef41f59422589a2e15630c

---
+++ sql/updates/6264_world.sql
@@ -0,0 +1,12 @@
+UPDATE `creature_template` SET `spell5` = '45848' WHERE `creature_template`.`entry` = 25653;
+UPDATE `creature_template` SET `spell1` = '45862' WHERE `creature_template`.`entry` = 25653;
+UPDATE `creature_template` SET `spell4` = '0' WHERE `creature_template`.`entry` = 25653;
+UPDATE `creature_template` SET `spell2` = '45856' WHERE `creature_template`.`entry` = 25653;
+
+#Soldat du soleil brisé :
+REPLACE INTO creature_equip_template VALUES (3005,38099,48068,0,33490690,1540,0,781,14,0);
+UPDATE creature_template SET equipment_id = 3005 WHERE entry = 26259;
+
+#Ouvrefaille du soleil brisé :
+REPLACE INTO creature_equip_template VALUES (91534,47909,0,0,33490690,0,0,781,0,0);
+UPDATE creature_template SET equipment_id = 91534 WHERE entry = 26289; 
