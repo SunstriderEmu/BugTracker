94497b1252f15ea57729ea12c5efb3e828b53b01
Kelno <kelnowm@gmail.com>
2014-02-14 09:43:14 +0100

    Updated npc_arcane_anomaly + Shadowmoon Houndmaster SAI + Shadowmoon Riding Hound SAI + increased FLAME_ENRAGE_DISTANCE on Illidan (P2) + ScriptedAI::DoCast should be usable without a given victim
    Former-commit-id: f4fa91e4d1ede15578267b38686c71f7e9b7152c

--- sql/updates/6330_world.sql
+++ sql/updates/6330_world.sql
@@ -19,14 +19,14 @@ UPDATE `creature_template` SET `AIName`="SmartAI", ScriptName = "" WHERE `entry`
 DELETE FROM eventai_scripts WHERE creature_id = @ENTRY;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=0;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
-(@ENTRY,0,0,0,4,0,100,2,0,0,0,0,11,39906,0,0,0,0,0,1,0,0,0,0,0,0,0,"Shadowmoon Houndmaster - On Aggro - Cast 'Summon Riding Warhound' (Normal Dungeon)"),
-(@ENTRY,0,1,0,4,0,100,2,0,0,0,0,11,41085,0,0,0,0,0,2,0,0,0,0,0,0,0,"Shadowmoon Houndmaster - On Aggro - Cast 'Freezing Trap' (Normal Dungeon)"),
-(@ENTRY,0,2,0,9,0,100,2,5,30,1500,3000,11,41093,0,0,0,0,0,2,0,0,0,0,0,0,0,"Shadowmoon Houndmaster - Within 5-30 Range - Cast 'Shoot' (Normal Dungeon)"),
-(@ENTRY,0,3,0,0,0,100,2,3500,3500,15000,20000,11,41084,0,0,0,0,0,5,0,0,0,0,0,0,0,"Shadowmoon Houndmaster - In Combat - Cast 'Silencing Shot' (Normal Dungeon)"),
-(@ENTRY,0,4,0,0,0,100,2,20000,20000,45000,50000,11,41091,0,0,0,0,0,5,0,0,0,0,0,0,0,"Shadowmoon Houndmaster - In Combat - Cast 'Volley' (Normal Dungeon)"),
-(@ENTRY,0,5,0,0,0,100,2,2000,3000,10000,15000,11,32908,0,0,0,0,0,2,0,0,0,0,0,0,0,"Shadowmoon Houndmaster - In Combat - Cast 'Wing Clip' (Normal Dungeon)"),
-(@ENTRY,0,6,0,4,0,100,2,0,0,0,0,43,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"Shadowmoon Houndmaster - On Aggro - Dismount (Normal Dungeon)"),
-(@ENTRY,0,7,0,25,0,100,2,0,0,0,0,43,0,14334,0,0,0,0,0,0,0,0,0,0,0,0,"Shadowmoon Houndmaster - On Reset - Mount To Model 14334 (Normal Dungeon)");
+(@ENTRY,0,0,0,4,0,100,0,0,0,0,0,11,39906,0,0,0,0,0,1,0,0,0,0,0,0,0,"Shadowmoon Houndmaster - On Aggro - Cast 'Summon Riding Warhound'"),
+(@ENTRY,0,1,0,4,0,100,0,0,0,0,0,11,41085,0,0,0,0,0,2,0,0,0,0,0,0,0,"Shadowmoon Houndmaster - On Aggro - Cast 'Freezing Trap'"),
+(@ENTRY,0,2,0,9,0,100,0,5,30,1500,3000,11,41093,0,0,0,0,0,2,0,0,0,0,0,0,0,"Shadowmoon Houndmaster - Within 5-30 Range - Cast 'Shoot'"),
+(@ENTRY,0,3,0,0,0,100,0,3500,3500,15000,20000,11,41084,0,0,0,0,0,5,0,0,0,0,0,0,0,"Shadowmoon Houndmaster - In Combat - Cast 'Silencing Shot'"),
+(@ENTRY,0,4,0,0,0,100,0,20000,20000,45000,50000,11,41091,0,0,0,0,0,5,0,0,0,0,0,0,0,"Shadowmoon Houndmaster - In Combat - Cast 'Volley'"),
+(@ENTRY,0,5,0,0,0,100,0,2000,3000,10000,15000,11,32908,0,0,0,0,0,2,0,0,0,0,0,0,0,"Shadowmoon Houndmaster - In Combat - Cast 'Wing Clip'"),
+(@ENTRY,0,6,0,4,0,100,0,0,0,0,0,43,0,0,0,0,0,0,1,0,0,0,0,0,0,0,"Shadowmoon Houndmaster - On Aggro - Dismount"),
+(@ENTRY,0,7,0,7,0,100,0,0,0,0,0,43,0,14334,0,0,0,0,1,0,0,0,0,0,0,0,"Shadowmoon Houndmaster - On Evade - Mount To Model 14334");
 
 -- Shadowmoon Riding Hound SAI
 SET @ENTRY := 23083;
@@ -34,10 +34,10 @@ UPDATE `creature_template` SET `AIName`="SmartAI", ScriptName = "" WHERE `entry`
 DELETE FROM eventai_scripts WHERE creature_id = @ENTRY;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=0;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
-(@ENTRY,0,0,0,4,0,100,3,0,0,0,0,11,25821,0,0,0,0,0,2,0,0,0,0,0,0,0,"Shadowmoon Riding Hound - On Aggro - Cast 'Charge' (No Repeat) (Normal Dungeon)"),
-(@ENTRY,0,1,0,0,0,100,2,3000,6000,20000,25000,11,41092,0,0,0,0,0,2,0,0,0,0,0,0,0,"Shadowmoon Riding Hound - In Combat - Cast 'Carnivorous Bite' (Normal Dungeon)"),
-(@ENTRY,0,2,0,2,0,100,3,0,30,0,0,11,8599,0,0,0,0,0,1,0,0,0,0,0,0,0,"Shadowmoon Riding Hound - Between 0-30% Health - Cast 'Enrage' (No Repeat) (Normal Dungeon)"),
-(@ENTRY,0,3,0,2,0,100,3,0,30,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,"Shadowmoon Riding Hound - Between 0-30% Health - Say Line 0 (No Repeat) (Normal Dungeon)"),
-(@ENTRY,0,4,0,7,0,100,2,0,0,0,0,37,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"Shadowmoon Riding Hound - On Evade - Kill Self (Normal Dungeon)");
+(@ENTRY,0,0,0,4,0,100,1,0,0,0,0,11,25821,0,0,0,0,0,2,0,0,0,0,0,0,0,"Shadowmoon Riding Hound - On Aggro - Cast 'Charge' (No Repeat)"),
+(@ENTRY,0,1,0,0,0,100,0,3000,6000,20000,25000,11,41092,0,0,0,0,0,2,0,0,0,0,0,0,0,"Shadowmoon Riding Hound - In Combat - Cast 'Carnivorous Bite'"),
+(@ENTRY,0,2,0,2,0,100,1,0,30,0,0,11,8599,0,0,0,0,0,1,0,0,0,0,0,0,0,"Shadowmoon Riding Hound - Between 0-30% Health - Cast 'Enrage' (No Repeat)"),
+(@ENTRY,0,3,0,2,0,100,1,0,30,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,"Shadowmoon Riding Hound - Between 0-30% Health - Say Line 0 (No Repeat)"),
+(@ENTRY,0,4,0,7,0,100,0,0,0,0,0,41,0,0,0,0,0,0,1,0,0,0,0,0,0,0,"Shadowmoon Riding Hound - On Evade - Despawn Instant");
 
 UPDATE creature_template SET AIName = "", ScriptName = "npc_arcane_anomaly" WHERE entry = 16488;
