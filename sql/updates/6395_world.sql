d67bfda81c9579ab2bfbf80a703b307b2df7b098
Kelno <kelnowm@gmail.com>
2014-02-22 21:53:21 +0100

    Displaced new message event system from ScriptedAI to Creature + updated some trashes in slave pen + fixed item 28785 proc using wrong max level
    Former-commit-id: 61f6c3b33e9c9d7f55bdd2bfd703d897bb84fd03

--- sql/updates/6395_world.sql
+++ sql/updates/6395_world.sql
@@ -1,5 +1,9 @@
-DELETE FROM creature_template_addon WHERE entry =17964;
-update creature_template set AIName = "", ScriptName = "npc_wastewalker_worker" where entry = 17964;
+DELETE FROM creature_template_addon WHERE entry IN (17963,17964);
+update creature_template set ScriptName = "npc_wastewalker_worker" where entry IN (17963,17964);
 
-SET @ENTRY := 17959;
-UPDATE `creature_template` SET `AIName`="SmartAI",`ScriptName`="npc_coilfang_slavehandler" WHERE `entry`=@ENTRY;
+REPLACE INTO `creature_text` VALUES (17959,0,0,'Get back to work you!','Vous, retournez au travail !',1,0,100,0,0,0,'Conducteur esclaves de Glissecroc - Enclos'),(17959,0,1,'Wake up! Now get up and back to work!','Debout ! On se lève et on retourne au travail !',1,0,100,0,0,0,'Conducteur esclaves de Glissecroc - Enclos'),(17959,0,5,'What is this?! Didn\'t mommy and daddy teach you anything?!','Hé ben alors ? Papa et môman ne t\'ont rien appris ?',1,0,100,0,0,0,'Conducteur esclaves de Glissecroc - Enclos'),(17959,1,0,'Assist me slaves!','Esclaves, aidez-moi !',1,0,100,0,0,0,'Conducteur esclaves de Glissecroc - Aggro');
+
+-- Coilfang Slavehandler 
+UPDATE `creature_template` SET `AIName`="",`ScriptName`="npc_coilfang_slavehandler" WHERE `entry`=17959;
+
+UPDATE spell_template SET maxLevel = 0 WHERE id = 37661;
