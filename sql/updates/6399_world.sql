9bdd2344d31ba004fdc9ef99d77d7c27aaba8132
Kelno <kelnowm@gmail.com>
2014-02-23 14:39:20 +0100

    Reverted recent change with UNIT_FLAG_NON_ATTACKABLE on Felmyst
    Former-commit-id: b27d85f1d0b64c5a4b65f63355c25c9c0f43c809

---
+++ sql/updates/6399_world.sql
@@ -0,0 +1,3 @@
+REPLACE INTO command VALUES ("bg",0,"Syntaxe : .bg goulet/arathi/alterac/cyclone\n\nAjouter \"groupe\" après le nom du bg pour inscrire le groupe. (exemple : .bg arathi groupe)",0);
+
+UPDATE creature_template SET flags_extra = 2 WHERE entry = 17518;
