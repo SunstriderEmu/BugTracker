c59c3c2b9cc99dc5f5dc5072e3791620cf4dfc83
Kelno <kelnowm@gmail.com>
2014-02-28 15:13:33 +0100

    Removed giant lock from Player::SaveToDB() as this seem to be causing deadlocks + allowing most triggered spell to be cast without a caster
    Former-commit-id: c8c835b08533bd738e3056dfdfd607df6f92980c

--- sql/updates/old/5290_world.sql
+++ sql/updates/old/5290_world.sql
@@ -1,9 +1,9 @@
 delete from `script_texts` where entry >= -1999932 and entry <= -1999926;
 
-insert into `script_texts` (`entry`, `content_default`, `content_loc2`) values('-1999932','There\'s just never enough Mojo to go around... ','Oh $N, quand j\'en aurai fini avec toi...');
-insert into `script_texts` (`entry`, `content_default`, `content_loc2`) values('-1999931','Your lily pad or mine?','Ton nénuphar ou le mien?');
-insert into `script_texts` (`entry`, `content_default`, `content_loc2`) values('-1999930','Now that\'s what I call froggy-style !','Je veux t\'entendre croasser...');
-insert into `script_texts` (`entry`, `content_default`, `content_loc2`) values('-1999929','This won\'t take long, did it?','Ca ne sera pas long, pas vrai?');
-insert into `script_texts` (`entry`, `content_default`, `content_loc2`) values('-1999928','I promise not to give you warts...','Je promets de ne pas te donner de verrues...');
-insert into `script_texts` (`entry`, `content_default`, `content_loc2`) values('-1999927','I thought you\'d ever ask !','Je commencais à croire que tu ne demanderais jamais !');
-insert into `script_texts` (`entry`, `content_default`, `content_loc2`) values('-1999926','Listen, $N. I know of a little swamp not too far from here...','Tu sais $N... Je connais un petit marais pas loin...');
+replace into `script_texts` (`entry`, `content_default`, `content_loc2`) values('-1999932','There\'s just never enough Mojo to go around... ','Oh $N, quand j\'en aurai fini avec toi...');
+replace into `script_texts` (`entry`, `content_default`, `content_loc2`) values('-1999931','Your lily pad or mine?','Ton nÃ©nuphar ou le mien ?');
+replace into `script_texts` (`entry`, `content_default`, `content_loc2`) values('-1999930','Now that\'s what I call froggy-style !','Je veux t\'entendre croasser...');
+replace into `script_texts` (`entry`, `content_default`, `content_loc2`) values('-1999929','This won\'t take long, did it?','Ca ne sera pas long, pas vrai?');
+replace into `script_texts` (`entry`, `content_default`, `content_loc2`) values('-1999928','I promise not to give you warts...','Je promets de ne pas te donner de verrues...');
+replace into `script_texts` (`entry`, `content_default`, `content_loc2`) values('-1999927','I thought you\'d ever ask !','Je commencais Ã  croire que tu ne demanderais jamais !');
+replace into `script_texts` (`entry`, `content_default`, `content_loc2`) values('-1999926','Listen, $N. I know of a little swamp not too far from here...','Tu sais $N... Je connais un petit marais pas loin...');
