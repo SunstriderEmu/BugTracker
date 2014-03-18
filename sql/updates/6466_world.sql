44d0caf7423c172e83cf928f5074c20296c94f11
Kelno <kelnowm@gmail.com>
2014-03-18 05:57:02 +0100

    Minor fixes on KJ + Only play Twins intro once + changed spell speed calculation
    Former-commit-id: b7edd056af20d5979464274a4f78a494cd30dc82

--- sql/updates/6466_world.sql
+++ sql/updates/6466_world.sql
@@ -1,1 +1,4 @@
+#Shadow Sight can't miss
 UPDATE spell_template SET AttributesEx3 = 0x00040000 WHERE id = 34709;
+#Allowing KJ Armageddon to target any enemy unit
+UPDATE spell_template SET effectImplicitTargetA1 = 6 WHERE id = 45921;
