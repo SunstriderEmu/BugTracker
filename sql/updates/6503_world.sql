040411c6119b898c74f1bf3a57e14b312b6cb7c5
Kelno <kelnowm@gmail.com>
2014-03-27 01:08:26 +0100

    Spell healing power can actually be negative + updated some heroic creatures damage by Meekon
    Former-commit-id: ac3da9187e01c577d7554c9aed0ad2f99e7ed876

---
+++ sql/updates/6503_world.sql
@@ -0,0 +1,362 @@
+#Remparts
+#Affameur mâche-les-os
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17259;
+UPDATE `creature_template` SET `mindmg` = '1781', `maxdmg` = '2563' WHERE `entry` = 18053;
+ 
+#Vorace mâche-les-os
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17264;
+UPDATE `creature_template` SET `mindmg` = '1698', `maxdmg` = '5012' WHERE `entry` = 18054;
+ 
+#Chien de la guerre de la Main brisée
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17280;
+UPDATE `creature_template` SET `mindmg` = '1611', `maxdmg` = '2365' WHERE `entry` = 18059;
+ 
+#Destructeur mâche-les-os
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17271;
+UPDATE `creature_template` SET `mindmg` = '2289', `maxdmg` = '3323' WHERE `entry` = 17043;
+ 
+#Archer de l'Orbite-sanglante
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17270;
+UPDATE `creature_template` SET `mindmg` = '1347', `maxdmg` = '1983' WHERE `entry` = 18048;
+ 
+#Invocateur noir de l'Orbite-sanglante
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17269;
+UPDATE `creature_template` SET `mindmg` = '1128', `maxdmg` = '1651' WHERE `entry` = 18049;
+ 
+#Belluaire mâche-les-os
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17455;
+UPDATE `creature_template` SET `mindmg` = '1367', `maxdmg` = '1962' WHERE `entry` = 18051;
+ 
+#Eventreur mâche-les-os
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17455;
+UPDATE `creature_template` SET `mindmg` = '1367', `maxdmg` = '1962' WHERE `entry` = 18051;
+ 
+#Clairvoyant de l'Orbite-sanglante
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17478;
+UPDATE `creature_template` SET `mindmg` = '688', `maxdmg` = '991' WHERE `entry` = 18055;
+ 
+#Factionnaire des Flammes infernales
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17517;
+UPDATE `creature_template` SET `mindmg` = '399', `maxdmg` = '1119' WHERE `entry` = 18057;
+ 
+#Gardien des guetteurs Gargolmar
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17306;
+UPDATE `creature_template` SET `mindmg` = '1399', `maxdmg` = '2026' WHERE `entry` = 18436;
+ 
+#Guetteur des Flammes infernales
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17309;
+UPDATE `creature_template` SET `mindmg` = '673', `maxdmg` = '973' WHERE `entry` = 18058;
+ 
+#Omor l'Intouché
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17308;
+UPDATE `creature_template` SET `mindmg` = '1621', `maxdmg` = '2330' WHERE `entry` = 18433;
+ 
+#Molosse diabolique
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17540;
+UPDATE `creature_template` SET `mindmg` = '1206', `maxdmg` = '1752' WHERE `entry` = 18056;
+ 
+#Vazruden
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17537;
+UPDATE `creature_template` SET `mindmg` = '2799', `maxdmg` = '4019' WHERE `entry` = 18434;
+ 
+#Nazan
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17536;
+UPDATE `creature_template` SET `mindmg` = '2335', `maxdmg` = '3380' WHERE `entry` = 18432;
+ 
+ 
+#Méchanar
+#Pourfendeur garde-sang :
+UPDATE `creature_template` SET `mindmg` = '631', `maxdmg` = '906' WHERE `entry` = 19167;
+UPDATE `creature_template` SET `mindmg` = '2253', `maxdmg` = '3269' WHERE `entry` = 21524;
+ 
+#Médecin garde-sang :
+UPDATE `creature_template` SET `mindmg` = '606', `maxdmg` = '867' WHERE `entry` = 20990;
+UPDATE `creature_template` SET `mindmg` = '1207', `maxdmg` = '1751' WHERE `entry` = 21523;
+ 
+#Lieuse de néant :
+UPDATE `creature_template` SET `mindmg` = '581', `maxdmg` = '835' WHERE `entry` = 20059;
+UPDATE `creature_template` SET `mindmg` = '1124', `maxdmg` = '1620' WHERE `entry` = 21541;
+#Centurion garde-sang
+UPDATE `creature_template` SET `mindmg` = '634', `maxdmg` = '908' WHERE `entry` = 19510;
+UPDATE `creature_template` SET `mindmg` = '1599', `maxdmg` = '2302' WHERE `entry` = 21522;
+ 
+#Patrouilleur forge-tempête :
+UPDATE `creature_template` SET `mindmg` = '793', `maxdmg` = '1140' WHERE entry = 19166;
+UPDATE `creature_template` SET `mindmg` = '4055', `maxdmg` = '5856' WHERE entry = 21543;
+ 
+#Destructeur forge-tempête :
+UPDATE `creature_template` SET `mindmg` = '1125', `maxdmg` = '1601' WHERE entry = 19735;
+UPDATE `creature_template` SET `mindmg` = '5112', `maxdmg` = '7410' WHERE entry = 21542;
+ 
+#Astromage cherche-soleil :
+UPDATE `creature_template` SET `mindmg` = '606', `maxdmg` = '868' WHERE entry = 19168;
+UPDATE `creature_template` SET `mindmg` = '1206', `maxdmg` = '1750' WHERE entry = 21539;
+ 
+#Gardien de porte Gyro-Meurtre :
+UPDATE `creature_template` SET `mindmg` = '1385', `maxdmg` = '2011' WHERE entry = 19218;
+UPDATE `creature_template` SET `mindmg` = '3055', `maxdmg` = '4425' WHERE entry = 21525;
+ 
+#Casseur du Méchanar :
+UPDATE `creature_template` SET `mindmg` = '781', `maxdmg` = '1114' WHERE entry = 19713;
+UPDATE `creature_template` SET `mindmg` = '1726', `maxdmg` = '2486' WHERE entry = 21532;
+ 
+#Perforateur du Méchanar :
+UPDATE `creature_template` SET `mindmg` = '798', `maxdmg` = '1119' WHERE entry = 19716;
+UPDATE `creature_template` SET `mindmg` = '1749', `maxdmg` = '2513' WHERE entry = 21531;
+ 
+#Ingénieur cherche-soleil :
+UPDATE `creature_template` SET `mindmg` = '606', `maxdmg` = '1218' WHERE entry = 20988;
+UPDATE `creature_template` SET `mindmg` = '1556', `maxdmg` = '2660' WHERE entry = 21540;
+ 
+#Mécanoseigneur Capacitus :
+UPDATE `creature_template` SET `mindmg` = '1398', `maxdmg` = '2006' WHERE entry = 19219;
+UPDATE `creature_template` SET `mindmg` = '1623', `maxdmg` = '2223' WHERE entry = 21533;
+ 
+#Gardien de porte en Main-de-fer :
+UPDATE `creature_template` SET `mindmg` = '1386', `maxdmg` = '2007' WHERE entry = 19710;
+UPDATE `creature_template` SET `mindmg` = '3058', `maxdmg` = '4418' WHERE entry = 21526;
+ 
+#Néantomancienne Sepethrea :
+UPDATE `creature_template` SET `mindmg` = '1123', `maxdmg` = '1623' WHERE entry = 19221;
+UPDATE `creature_template` SET `mindmg` = '2319', `maxdmg` = '3354' WHERE entry = 21536;
+ 
+#Flammes violentes :
+UPDATE `creature_template` SET `mindmg` = '520', `maxdmg` = '752' WHERE entry = 20481;
+UPDATE `creature_template` SET `mindmg` = '539', `maxdmg` = '781' WHERE entry = 21538;
+ 
+#Pathaleon le Calculateur :
+UPDATE `creature_template` SET `mindmg` = '609', `maxdmg` = '858' WHERE entry = 19220;
+UPDATE `creature_template` SET `mindmg` = '1397', `maxdmg` = '2118' WHERE entry = 21537;
+ 
+#Ame en peine du Néant :
+UPDATE `creature_template` SET `mindmg` = '435', `maxdmg` = '779' WHERE entry = 21062;
+UPDATE `creature_template` SET `mindmg` = '447', `maxdmg` = '807' WHERE entry = 21535;
+ 
+ 
+ 
+#Enclos
+#Matrok
+UPDATE `creature_template` SET `mindmg` = '753', `maxdmg` = '1071' WHERE `entry` = 17816;
+UPDATE `creature_template` SET `mindmg` = '2106', `maxdmg` = '3011' WHERE `entry` = 19884;
+ 
+#Matrok supérieur
+UPDATE `creature_template` SET `mindmg` = '754', `maxdmg` = '1070' WHERE `entry` = 17817;
+UPDATE `creature_template` SET `mindmg` = '3464', `maxdmg` = '4973' WHERE `entry` = 19892;
+ 
+#Champion de Glissecroc
+UPDATE `creature_template` SET `mindmg` = '705', `maxdmg` = '1003' WHERE `entry` = 17957;
+UPDATE `creature_template` SET `mindmg` = '3010', `maxdmg` = '4345' WHERE `entry` = 19885;
+ 
+#Enchanteresse de Glissecroc
+UPDATE `creature_template` SET `mindmg` = '746', `maxdmg` = '1059' WHERE `entry` = 17961;
+UPDATE `creature_template` SET `mindmg` = '1063', `maxdmg` = '1538' WHERE `entry` = 19887;
+ 
+#Raie de Glissecroc
+UPDATE `creature_template` SET `mindmg` = '609', `maxdmg` = '852' WHERE `entry` = 21128;
+UPDATE `creature_template` SET `mindmg` = '2598', `maxdmg` = '3755' WHERE `entry` = 21841;
+ 
+#Conducteur d'esclaves de Glissecroc
+UPDATE `creature_template` SET `mindmg` = '735', `maxdmg` = '1059' WHERE `entry` = 17959;
+UPDATE `creature_template` SET `mindmg` = '4032', `maxdmg` = '5793' WHERE `entry` = 19889;
+ 
+#Observateur de Glissecroc
+UPDATE `creature_template` SET `mindmg` = '762', `maxdmg` = '1058' WHERE `entry` = 17938;
+UPDATE `creature_template` SET `mindmg` = '1412', `maxdmg` = '2025' WHERE `entry` = 19888;
+ 
+#Soigne-écaille de Glissecroc
+UPDATE `creature_template` SET `mindmg` = '630', `maxdmg` = '984' WHERE `entry` = 21126;
+UPDATE `creature_template` SET `mindmg` = '861', `maxdmg` = '1275' WHERE `entry` = 21842;
+ 
+#Ouvrier Marchefriche
+UPDATE `creature_template` SET `mindmg` = '105', `maxdmg` = '151' WHERE `entry` = 17964;
+UPDATE `creature_template` SET `mindmg` = '625', `maxdmg` = '910' WHERE `entry` = 19904;
+ 
+#Esclave Marchefriche
+UPDATE `creature_template` SET `mindmg` = '160', `maxdmg` = '231' WHERE `entry` = 17963;
+UPDATE `creature_template` SET `mindmg` = '1285', `maxdmg` = '1463' WHERE `entry` = 19902;
+ 
+#Devineresse de Glissecroc
+UPDATE `creature_template` SET `mindmg` = '693', `maxdmg` = '982' WHERE `entry` = 17960;
+UPDATE `creature_template` SET `mindmg` = '811', `maxdmg` = '1200' WHERE `entry` = 19890;
+ 
+#Défenseur de Glissecroc
+UPDATE `creature_template` SET `mindmg` = '705', `maxdmg` = '1005' WHERE `entry` = 17958;
+UPDATE `creature_template` SET `mindmg` = '4431', `maxdmg` = '6369' WHERE `entry` = 19886;
+ 
+#Collaborateur de Glissecroc
+UPDATE `creature_template` SET `mindmg` = '451', `maxdmg` = '643' WHERE `entry` = 17962;
+UPDATE `creature_template` SET `mindmg` = '1558', `maxdmg` = '2264' WHERE `entry` = 19903;
+ 
+#Technicien de Glissecroc
+UPDATE `creature_template` SET `mindmg` = '351', `maxdmg` = '996' WHERE `entry` = 17940;
+UPDATE `creature_template` SET `mindmg` = '435', `maxdmg` = '1269' WHERE `entry` = 19891;
+ 
+#Mennu le Traître
+UPDATE `creature_template` SET `mindmg` = '574', `maxdmg` = '816' WHERE `entry` = 17941;
+UPDATE `creature_template` SET `mindmg` = '1395', `maxdmg` = '2024' WHERE `entry` = 19893;
+ 
+#Rokmar le Crépitant
+UPDATE `creature_template` SET `mindmg` = '1080', `maxdmg` = '1575' WHERE `entry` = 17991;
+UPDATE `creature_template` SET `mindmg` = '3628', `maxdmg` = '4822' WHERE `entry` = 19895;
+ 
+#Bourbierreux
+UPDATE `creature_template` SET `mindmg` = '2028', `maxdmg` = '2683' WHERE `entry` = 17942;
+UPDATE `creature_template` SET `mindmg` = '4579', `maxdmg` = '6638' WHERE `entry` = 19894;
+ 
+ 
+ 
+#Caveau
+#Guerrier de Glissecroc
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17802;
+UPDATE `creature_template` SET `mindmg` = '2998', `maxdmg` = '4561' WHERE `entry` = 20626;
+ 
+#Surgisseur de vapeur
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 21696;
+UPDATE `creature_template` SET `mindmg` = '1422', `maxdmg` = '2004' WHERE `entry` = 21916;
+ 
+#Ingénieur de Glissecroc
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17721;
+UPDATE `creature_template` SET `mindmg` = '1332', `maxdmg` = '1872' WHERE `entry` = 20620;
+ 
+#Surgisseur de marée
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 21695;
+UPDATE `creature_template` SET `mindmg` = '2274', `maxdmg` = '3209' WHERE `entry` = 21917;
+ 
+#Oracle de Glissecroc
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17803;
+UPDATE `creature_template` SET `mindmg` = '1024', `maxdmg` = '1509' WHERE `entry` = 20622;
+ 
+#Sirène de Glissecroc
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17801;
+UPDATE `creature_template` SET `mindmg` = '2487', `maxdmg` = '2804' WHERE `entry` = 20623;
+ 
+#Sorcière de Glissecroc
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17722;
+UPDATE `creature_template` SET `mindmg` = '1146', `maxdmg` = '1605' WHERE `entry` = 20625;
+ 
+#Myrmidon de Glissecroc
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17800;
+UPDATE `creature_template` SET `mindmg` = '2635', `maxdmg` = '4024' WHERE `entry` = 20621;
+ 
+#Haut seigneur-tourbe
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 21694;
+UPDATE `creature_template` SET `mindmg` = '5528', `maxdmg` = '7779' WHERE `entry` = 21914;
+ 
+#Elève crapuche
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17799;
+UPDATE `creature_template` SET `mindmg` = '780', `maxdmg` = '1131' WHERE `entry` = 20628;
+ 
+#Maître d'esclaves de Glissecroc
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17805;
+UPDATE `creature_template` SET `mindmg` = '4335', `maxdmg` = '6252' WHERE `entry` = 20624;
+ 
+#Lépreux de Glissecroc
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 21338;
+UPDATE `creature_template` SET `mindmg` = '781', `maxdmg` = '1125' WHERE `entry` = 21915;
+ 
+#Elémentaire d'eau de Glissecroc
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17917;
+UPDATE `creature_template` SET `mindmg` = '1038', `maxdmg` = '1503' WHERE `entry` = 20627;
+ 
+#Hydromancienne Thespia
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17797;
+UPDATE `creature_template` SET `mindmg` = '2322', `maxdmg` = '3372' WHERE `entry` = 20629;
+ 
+#Mécanicien montevapeur
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17951;
+UPDATE `creature_template` SET `mindmg` = '305', `maxdmg` = '625' WHERE `entry` = 20632;
+ 
+#Mekgénieur Montevapeur
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17796;
+UPDATE `creature_template` SET `mindmg` = '3101', `maxdmg` = '4660' WHERE `entry` = 20630;
+ 
+#Seigneur de guerre Kalithresh
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17796;
+UPDATE `creature_template` SET `mindmg` = '3105', `maxdmg` = '4569' WHERE `entry` = 20630;
+ 
+ 
+ 
+#Basse-tourbière
+#Basse-tourboptère
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17724;
+UPDATE `creature_template` SET `mindmg` = '2089', `maxdmg` = '3014' WHERE `entry` = 20185;
+ 
+#Rôdeur de Basse-tourbière
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17725;
+UPDATE `creature_template` SET `mindmg` = '1693', `maxdmg` = '2433' WHERE `entry` = 20188;
+ 
+#Traînard de Basse-tourbière
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17871;
+UPDATE `creature_template` SET `mindmg` = '3684', `maxdmg` = '5291' WHERE `entry` = 20190;
+ 
+#Géant des tourbières
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17723;
+UPDATE `creature_template` SET `mindmg` = '3000', `maxdmg` = '3525' WHERE `entry` = 20164;
+ 
+#Myrmidon Iraileron
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17726;
+UPDATE `creature_template` SET `mindmg` = '3347', `maxdmg` = '4852' WHERE `entry` = 20191;
+ 
+#Factionnaire Iraileron
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17727;
+UPDATE `creature_template` SET `mindmg` = '3005', `maxdmg` = '4343' WHERE `entry` = 20192;
+ 
+#Membre de la tribu bourbesang
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17728;
+UPDATE `creature_template` SET `mindmg` = '788', `maxdmg` = '2298' WHERE `entry` = 20181;
+ 
+#Porte-épieu bourbesang
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17729;
+UPDATE `creature_template` SET `mindmg` = '1620', `maxdmg` = '1913' WHERE `entry` = 20180;
+ 
+#Guerrieur Iraileron
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17735;
+UPDATE `creature_template` SET `mindmg` = '1592', `maxdmg` = '2489' WHERE `entry` = 20193;
+ 
+#Soigneur bourbesang
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17730;
+UPDATE `creature_template` SET `mindmg` = '1049', `maxdmg` = '1527' WHERE `entry` = 20177;
+ 
+#Oracle bourbesang
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17771;
+UPDATE `creature_template` SET `mindmg` = '1240', `maxdmg` = '1410' WHERE `entry` = 20179;
+ 
+#Raie des tourbières
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17731;
+UPDATE `creature_template` SET `mindmg` = '1586', `maxdmg` = '2299' WHERE `entry` = 20173;
+ 
+#Dard lykul
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 19632;
+UPDATE `creature_template` SET `mindmg` = '2823', `maxdmg` = '4077' WHERE `entry` = 20174;
+ 
+#Guêpe lykul
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17732;
+UPDATE `creature_template` SET `mindmg` = '2295', `maxdmg` = '3074' WHERE `entry` = 20175;
+ 
+#Seigneur de Basse-tourbière
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17734;
+UPDATE `creature_template` SET `mindmg` = '5850', `maxdmg` = '6450' WHERE `entry` = 20187;
+ 
+#Hungarfen
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17770;
+UPDATE `creature_template` SET `mindmg` = '3432', `maxdmg` = '4943' WHERE `entry` = 20169;
+ 
+#Ghaz'an
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 18105;
+UPDATE `creature_template` SET `mindmg` = '3024', `maxdmg` = '4368' WHERE `entry` = 20168;
+ 
+#Griffe
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17827;
+UPDATE `creature_template` SET `mindmg` = '1268', `maxdmg` = '1961' WHERE `entry` = 20165;
+ 
+#Seigneur des marais Musel'ek
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17826;
+UPDATE `creature_template` SET `mindmg` = '1216', `maxdmg` = '3498' WHERE `entry` = 20183;
+ 
+#La Traqueuse noire
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 17882;
+UPDATE `creature_template` SET `mindmg` = '2319', `maxdmg` = '3371' WHERE `entry` = 20184;
+ 
+#Trotteur des spores
+#UPDATE `creature_template` SET `mindmg` = '', `maxdmg` = '' WHERE `entry` = 22299;
+UPDATE `creature_template` SET `mindmg` = '375', `maxdmg` = '675' WHERE `entry` = 22300;
