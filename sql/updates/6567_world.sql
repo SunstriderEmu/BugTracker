bc9c0dee0aa324d666f9e459d8c0bea87f034cad
Kelno <kelnowm@gmail.com>
2014-05-21 18:34:14 +0200

    Updated some boss damages by Meekon
    Former-commit-id: 16141bae04794c048f2e2feee60c6b89cf85bb44

---
+++ sql/updates/6567_world.sql
@@ -0,0 +1,4 @@
+UPDATE `creature_template` SET `mindmg` = '19000', `maxdmg` = '22000', `baseattacktime` = '1100' WHERE `entry` = 24882;
+UPDATE `creature_template` SET `maxdmg` = '19000', `mindmg` = '16500' WHERE `entry` = 25038;
+UPDATE `creature_template` SET `maxdmg` = '30000', `mindmg` = '30000' WHERE `entry` = 25166;
+UPDATE `creature_template` SET `maxdmg` = '14000', `mindmg` = '18000' WHERE `entry` = 25166;
