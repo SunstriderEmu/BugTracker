f6bad134a1554d4439356ead8c8ac710b3afc12d
Kelno <kelnowm@gmail.com>
2014-08-16 21:04:09 +0200

    [TC] Fixed request item text for quest 6662
    Former-commit-id: ddc13ee676881da27921e5b34e1bddee2ec2eb86

---
+++ sql/updates/6666_world.sql
@@ -0,0 +1,1 @@
+UPDATE `quest_template` SET `RequestItemsText` = '<Nipsy puts his index finger through one of the air holes in the carton.>$b$bAlive and kicking... and just in time!' WHERE `entry` = 6662;
