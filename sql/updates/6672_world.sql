9010c61c17a7215614e0a50e593c173ae330da61
Kelno <kelnowm@gmail.com>
2014-08-17 00:05:51 +0200

    Re added some access requirements (Karazhan, SSC, The Eye & Black Temple)
    Former-commit-id: d7958e5b359848d257bdb7d6377a061dba084115

---
+++ sql/updates/6672_world.sql
@@ -0,0 +1,11 @@
+#Karazhan - The Master's Key
+UPDATE access_requirement SET item = 24490 where id = 27;
+
+#SSC - The Mark of Vashj
+UPDATE access_requirement SET quest_done = 10900 WHERE id = 37;
+
+#The Eye - The Tempest Key
+UPDATE access_requirement SET item = 31704 WHERE id = 38;
+
+#Black Temple - Medallion of Karabor
+UPDATE access_requirement SET item = 32649 WHERE id = 47;
