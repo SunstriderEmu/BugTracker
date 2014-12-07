2500c27fffca4a665dbc5e3688cf3d0c17ce78a0
Kelno <kelnowm@gmail.com>
2014-12-07 23:58:17 +0100

    Added some missing files in the libircclient update
    Former-commit-id: 099155c20ab7fe36ad3903a3da9b518cf02dea2b

--- sql/updates/6741_world.sql
+++ sql/updates/6741_world.sql
@@ -1,7 +1,11 @@
 #/!\ this update erase all attackpower in db
 #With this the creatures damages should exactly match the old values, this sql patch reflect the re introduction of db attackpower for creatures in the core side
+
+UPDATE creature_template SET baseattacktime = 2400 where baseattacktime < 50;
+UPDATE creature_template SET rangeattacktime = 2400 where rangeattacktime < 50;
+
 UPDATE creature_template SET baseattacktime = 2.4 WHERE baseattacktime = 0;
-UPDATE creature_template SET attackpower = ((mindmg + maxdmg) /6 / POWER(baseattacktime,2) * 14);
+UPDATE creature_template SET attackpower = ROUND((mindmg + maxdmg) /6 / POWER(baseattacktime/1000,2) * 14);
 
 UPDATE creature_template SET rangeattacktime = 2.4 WHERE rangeattacktime = 0;
-UPDATE creature_template SET rangedattackpower = ((minrangedmg + maxrangedmg) /6 / POWER(rangeattacktime,2) * 14);
+UPDATE creature_template SET rangedattackpower = ROUND((minrangedmg + maxrangedmg) /6 / POWER(rangeattacktime/1000,2) * 14);
