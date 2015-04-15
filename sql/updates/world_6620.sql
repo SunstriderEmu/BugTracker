5996324c65ba7f7d96ae3c57fefabf36e3d0e7d2
Kelno <kelnowm@gmail.com>
2015-04-15 17:30:42 +0200

    Fixed broken character rename opcode + Cleaned up all SMSG_CHAR_CREATE/SMSG_CHAR_DELETE and SMSG_CHAR_RENAME usages, now using proper WorldSession senders + Fixed broken shop (account_credits table was missing)
    Former-commit-id: 168adb7854a53bf60c45f3dd035c0e18f0e7975b

---
+++ sql/updates/world_6620.sql
@@ -0,0 +1,24 @@
+UPDATE trinity_string SET content_default = "You never had credits." WHERE entry = 11503;
+UPDATE shop_orders  SET name = "rename" WHERE id = 19;
+UPDATE shop_orders  SET name = "gold" WHERE id = 29;
+UPDATE shop_orders  SET name = "mount 1" WHERE id = 48;
+UPDATE shop_orders  SET name = "mount 2" WHERE id = 49;
+UPDATE shop_orders  SET name = "mount 3" WHERE id = 50;
+UPDATE shop_orders  SET name = "mount 4" WHERE id = 51;
+UPDATE shop_orders  SET name = "mount 5" WHERE id = 52;
+UPDATE shop_orders  SET name = "mount 6" WHERE id = 53;
+UPDATE shop_orders  SET name = "mount 7" WHERE id = 54;
+UPDATE shop_orders  SET name = "bag 1" WHERE id = 55;
+UPDATE shop_orders  SET name = "bag 2" WHERE id = 56;
+UPDATE shop_orders  SET name = "weapon 1" WHERE id = 57;
+UPDATE shop_orders  SET name = "weapon 2" WHERE id = 58;
+UPDATE shop_orders  SET name = "weapon 3" WHERE id = 59;
+UPDATE shop_orders  SET name = "weapon 4" WHERE id = 60;
+UPDATE shop_orders  SET name = "weapon 5" WHERE id = 61;
+UPDATE shop_orders  SET name = "weapon 6" WHERE id = 62;
+UPDATE shop_orders  SET name = "weapon 7" WHERE id = 63;
+UPDATE shop_orders  SET name = "weapon 8" WHERE id = 64;
+UPDATE shop_orders  SET name = "weapon 9" WHERE id = 65;
+UPDATE shop_orders  SET name = "character" WHERE (id BETWEEN 395 AND 403) OR (id BETWEEN 413 AND 416) OR (id BETWEEN 425 AND 446) OR (id BETWEEN 454 AND 457) OR (id BETWEEN 462 AND 474);
+UPDATE shop_orders  SET name = "bear mount" WHERE id = 487;
+#... not all done
