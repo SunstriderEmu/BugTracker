409745d1fd69cff938e0bce1b50800e32f85dc66
Kelno <kelnowm@gmail.com>
2015-04-22 17:46:52 +0200

    !yolo command + Fixed output for command ".quest totalcount"
    Former-commit-id: f9dc3f77a308b1645dda09272e69f7aab9a0cf0d

---
+++ sql/updates/world_6703.sql
@@ -0,0 +1,10 @@
+REPLACE INTO trinity_string  (entry, content_default) VALUES (11605, "Total quests completed this week: %u");
+REPLACE INTO trinity_string  (entry, content_default) VALUES (11606, "\
+        ROFL:ROFL:LOL:ROFL:ROFL\
+         _________||_________\
+ L      /                 [ O\\
+LOL=====                  |_|_\\
+ L     B|O M B                 )\
+      F |______,-------¬_____/\
+     T //    }-OMGROCKET))\
+    W // _______||_||_________/_/");
