eea42652733f8734e0f1b8b9b2fe03a8e817e335
Kelno <kelnowm@gmail.com>
2015-04-23 14:20:09 +0200

    Added night elf stealth boost as starting spell
    Former-commit-id: e69b55598636ce7c8c0f2a3fc05de0d9b13ca7e5

---
+++ sql/updates/world_2015_04_23_02.sql
@@ -0,0 +1,11 @@
+REPLACE INTO playercreateinfo_spell (race, class, Spell, Note, Active) VALUES (4,1,21009,"Passive improved stealth", 1);
+REPLACE INTO playercreateinfo_spell (race, class, Spell, Note, Active) VALUES (4,3,21009,"Passive improved stealth", 1);
+REPLACE INTO playercreateinfo_spell (race, class, Spell, Note, Active) VALUES (4,4,21009,"Passive improved stealth", 1);
+REPLACE INTO playercreateinfo_spell (race, class, Spell, Note, Active) VALUES (4,5,21009,"Passive improved stealth", 1);
+REPLACE INTO playercreateinfo_spell (race, class, Spell, Note, Active) VALUES (4,11,21009,"Passive improved stealth", 1);
+
+REPLACE INTO playercreateinfo_spell_custom (race, class, Spell, Note, Active) VALUES (4,1,21009,"Passive improved stealth", 1);
+REPLACE INTO playercreateinfo_spell_custom (race, class, Spell, Note, Active) VALUES (4,3,21009,"Passive improved stealth", 1);
+REPLACE INTO playercreateinfo_spell_custom (race, class, Spell, Note, Active) VALUES (4,4,21009,"Passive improved stealth", 1);
+REPLACE INTO playercreateinfo_spell_custom (race, class, Spell, Note, Active) VALUES (4,5,21009,"Passive improved stealth", 1);
+REPLACE INTO playercreateinfo_spell_custom (race, class, Spell, Note, Active) VALUES (4,11,21009,"Passive improved stealth", 1);
