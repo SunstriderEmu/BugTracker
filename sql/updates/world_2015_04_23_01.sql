3e766b2f712a810400bc4d6a3021af2a087b2d26
Kelno <kelnowm@gmail.com>
2015-04-23 12:48:20 +0200

    Renamed tables and related functions : creature_questrelation TO creature_queststarter; creature_involvedrelation TO creature_questender; gameobject_questrelation TO gameobject_queststarter; gameobject_involvedrelation TO gameobject_questender;
    Former-commit-id: b7b618bfd0f84a1c0c75c6082a863dae5da58f25

---
+++ sql/updates/world_2015_04_23_01.sql
@@ -0,0 +1,4 @@
+RENAME TABLE creature_questrelation TO creature_queststarter;
+RENAME TABLE creature_involvedrelation TO creature_questender;
+RENAME TABLE gameobject_questrelation TO gameobject_queststarter;
+RENAME TABLE gameobject_involvedrelation TO gameobject_questender;
