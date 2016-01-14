3e73a60ede6124bce1d30cc8b06d182292692b02
Kelno <kelnowm@gmail.com>
2016-01-15 00:12:16 +0100

    Fixed more database types at load
    Former-commit-id: 88b124f9f83a43b3ea7fa23caf0ae5616bb697dd

--- sql/updates/characters_2015_01_14_.sql
+++ sql/updates/characters_2015_01_14_.sql
@@ -1,2 +1,7 @@
 ALTER TABLE `game_event_save`   
   CHANGE `event_id` `event_id` INT(8) UNSIGNED NOT NULL;
+
+ALTER TABLE `character_skills`   
+  CHANGE `skill` `skill` INT(11) UNSIGNED NOT NULL,
+  CHANGE `value` `value` INT(11) UNSIGNED NOT NULL,
+  CHANGE `max` `max` INT(11) UNSIGNED NOT NULL;
--- sql/updates/world_2016_01_14_01.sql
+++ sql/updates/world_2016_01_14_01.sql
@@ -29,10 +29,20 @@ ALTER TABLE `spell_pet_auras`
   CHANGE `aura` `aura` INT(11) UNSIGNED NOT NULL   COMMENT 'pet aura id';
 
 ALTER TABLE `access_requirement`   
-  CHANGE `quest_failed_text` `quest_failed_text` INT(10) UNSIGNED DEFAULT 0  NOT NULL
+  CHANGE `quest_failed_text` `quest_failed_text` INT(10) UNSIGNED DEFAULT 0  NOT NULL,
   CHANGE `id` `id` INT(11) UNSIGNED NOT NULL   COMMENT 'Identifier';
 
 ALTER TABLE `areatrigger_teleport`   
   CHANGE `access_id` `access_id` INT(11) UNSIGNED DEFAULT 0  NOT NULL;
 
+ALTER TABLE `creature_onkill_reputation`   
+  CHANGE `RewOnKillRepFaction1` `RewOnKillRepFaction1` INT(11) UNSIGNED DEFAULT 0  NOT NULL,
+  CHANGE `RewOnKillRepFaction2` `RewOnKillRepFaction2` INT(11) UNSIGNED DEFAULT 0  NOT NULL,
+  CHANGE `RewOnKillRepValue1` `RewOnKillRepValue1` INT(11) DEFAULT 0  NOT NULL,
+  CHANGE `RewOnKillRepValue2` `RewOnKillRepValue2` INT(11) DEFAULT 0  NOT NULL;
   
+ALTER TABLE `game_event_creature`   
+  CHANGE `event` `event` INT(11) DEFAULT 0  NOT NULL;
+
+ALTER TABLE `item_loot_template`   
+  CHANGE `maxcount` `maxcount` TINYINT(3) UNSIGNED DEFAULT 1  NOT NULL;
