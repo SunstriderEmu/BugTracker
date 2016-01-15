cb3b9b569d62ffa69dd24282fb3a03a08ef4b5d7
Kelno <kelnowm@gmail.com>
2016-01-15 01:57:37 +0100

    ²
    Former-commit-id: 6a3c7507d516b8f89ba7f10bd07c7a630b6202d0

--- sql/updates/characters_2015_01_14_.sql
+++ sql/updates/characters_2015_01_14_.sql
@@ -5,3 +5,6 @@ ALTER TABLE `character_skills`
   CHANGE `skill` `skill` INT(11) UNSIGNED NOT NULL,
   CHANGE `value` `value` INT(11) UNSIGNED NOT NULL,
   CHANGE `max` `max` INT(11) UNSIGNED NOT NULL;
+
+ALTER TABLE `saved_variables`   
+  CHANGE `NextArenaPointDistributionTime` `NextArenaPointDistributionTime` BIGINT(40) UNSIGNED DEFAULT 1  NOT NULL;
