2bef061b78180432e9499b181b86c6de6040947b
Kelno <kelnowm@gmail.com>
2016-02-21 23:52:08 +0100

    Extended spellFamilyFlags column to BIGINT(20) and moved spellFamilyFlags2 data in it
    Former-commit-id: 17f5c4cbbe2d49d808927298af65f43f75592c9a

--- sql/updates/world_2016_02_21_01.sql
+++ sql/updates/world_2016_02_21_01.sql
@@ -1,3 +1,13 @@
 UPDATE creature c 
  JOIN creature_template ct ON c.id = ct.entry AND c.modelid = ct.modelid1
 SET c.modelid = 0;
+
+ALTER TABLE `spell_template`   
+  CHANGE `spellFamilyFlags1` `spellFamilyFlags` BIGINT(20) UNSIGNED DEFAULT 0  NOT NULL;
+
+UPDATE spell_template
+	SET spellFamilyFlags = spellFamilyFlags & (spellFamilyFlags2 * POWER(2,32))
+	WHERE spellFamilyFlags2 != 0;
+	
+ ALTER TABLE  `spell_template`
+	DROP COLUMN spellFamilyFlags2;
