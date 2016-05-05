a02698043a1c4d44d13c26dd50456f70354083e1
Kelno <kelnowm@gmail.com>
2016-05-05 18:03:18 +0200

    Added spell_area table + Added a bunch of missing const in Player.h
    Former-commit-id: 9a8ee488fbbe2a413daa7730ec8720673e6b1581

---
+++ sql/updates/world_2016_05_05_01.sql
@@ -0,0 +1,16 @@
+
+DROP TABLE IF EXISTS `spell_area`;
+
+CREATE TABLE `spell_area` (
+  `spell` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `area` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `quest_start` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `quest_end` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `aura_spell` mediumint(8) NOT NULL DEFAULT '0',
+  `racemask` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `gender` tinyint(3) unsigned NOT NULL DEFAULT '2',
+  `autocast` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `quest_start_status` int(11) NOT NULL DEFAULT '64',
+  `quest_end_status` int(11) NOT NULL DEFAULT '11',
+  PRIMARY KEY (`spell`,`area`,`quest_start`,`aura_spell`,`racemask`,`gender`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
