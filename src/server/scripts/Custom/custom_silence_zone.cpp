696a706918d56287454e3e77576afebe04484b2b
Kelno <kelnowm@gmail.com>
2017-01-04 23:07:46 +0100

    [TC] Core/World: Simplified sleep calculation routine for world update thread
    Former-commit-id: 31a5bad55128231b4f7a2bd90ec7f6c1bf5b5644

--- src/server/scripts/Custom/custom_silence_zone.cpp
+++ src/server/scripts/Custom/custom_silence_zone.cpp
@@ -2,12 +2,10 @@
 /* TODO : This won't work with multiple npc's (they'll conflift with each other)
 */
 
-/* ScriptData
-SDName: npc_zone_silence
-SD%Complete: 100
-SDComment: Custom NPC acting like a totem silencing nearby players
-SDCategory: Custom
-EndScriptData */
+/* 
+npc_zone_silence
+Custom NPC acting like a totem silencing nearby players
+ */
 
  
 #define SPELL_SILENCE 42201 //can only be cast on self
@@ -27,7 +25,7 @@ struct npc_zonedesilenceAI : public ScriptedAI
         if (who->GetTypeId() == TYPEID_PLAYER) {
             if (who->GetDistance(me) < SILENCE_RANGE ) {
                 if(!who->HasAuraEffect(SPELL_SILENCE, 0) && who->IsAlive())
-                    who->AddAura(SPELL_SILENCE, who); //pas un cast sinon rendement décroissant qui s'applique
+                    who->AddAura(SPELL_SILENCE, who); //not a cast, else rendement décroissant qui s'applique
             }
             else if(who->HasAuraEffect(SPELL_SILENCE, 0)) {
                 who->RemoveAurasDueToSpell(SPELL_SILENCE);
