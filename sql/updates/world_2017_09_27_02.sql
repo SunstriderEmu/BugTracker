06d4597f98212fa97f9ff7151095ae3389710588
Kelno <kelnowm@gmail.com>
2017-09-28 00:45:51 +0200

    Refactor sql folder structure
    Former-commit-id: 5622a2b5967a79b33fed969c67a85811a30b46a0

---
+++ sql/updates/world_2017_09_27_02.sql
@@ -0,0 +1,35 @@
+/*
+SQLyog Community v12.2.4 (64 bit)
+MySQL - 10.2.8-MariaDB : Database - trinityworld
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `updates_include` */
+
+DROP TABLE IF EXISTS `updates_include`;
+
+CREATE TABLE `updates_include` (
+  `path` varchar(200) NOT NULL COMMENT 'directory to include. $ means relative to the source directory.',
+  `state` enum('RELEASED','ARCHIVED') NOT NULL DEFAULT 'RELEASED' COMMENT 'defines if the directory contains released or archived updates.',
+  PRIMARY KEY (`path`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='List of directories where we want to include sql updates.';
+
+/*Data for the table `updates_include` */
+
+insert  into `updates_include`(`path`,`state`) values 
+('$/sql/updates/world','RELEASED'),
+('$/sql/custom/world','RELEASED'),
+('$/sql/old/3.3.5a/world','ARCHIVED');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
