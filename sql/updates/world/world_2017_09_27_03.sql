8b8193c2dd26129da9bf78224cc20355610541b9
Kelno <kelnowm@gmail.com>
2017-09-28 12:32:26 +0200

    Fixed database auto creation Moved older updates out of auto update system
    Former-commit-id: b71d6baa96732a5cdd62dfcac52e645718352dba

---
+++ sql/updates/world/world_2017_09_27_03.sql
@@ -0,0 +1,1 @@
+UPDATE trinity_string SET content_default = "Cannot queue, you or someone in your party is frozen by a gm" WHERE entry = 1134; 
---
+++ sql/updates/world/world_2017_09_27_04.sql
@@ -0,0 +1,2 @@
+
+UPDATE battleground_template SET MinPlayersPerTeam = 0 WHERE id = 4;
