97b49ab6baca01a9526e4adf2b856599ea8bc14c
Kelno <kelnowm@gmail.com>
2018-02-02 20:23:30 +0100

    Moved mage spells from OldWindrunnerHacks to SpellScript + Fixed Incanter's Regalia 4P spell
    Former-commit-id: 2e29c06cd8e5db769223c05e1ca668bd7d2947cc

--- sql/updates/world/world_2018_02_01_01.sql
+++ sql/updates/world/world_2018_02_01_01.sql
@@ -12,3 +12,16 @@ REPLACE INTO spell_script_names VALUES (37453, "spell_chess_3_front");
 REPLACE INTO spell_script_names VALUES (37459, "spell_chess_3_front");
 REPLACE INTO spell_script_names VALUES (37413, "spell_chess_front");
 REPLACE INTO spell_script_names VALUES (37406, "spell_chess_front");
+
+
+REPLACE INTO spell_script_names VALUES (-29441, "spell_mage_magic_absorption");
+REPLACE INTO spell_script_names VALUES (-29074, "spell_mage_master_of_elements");
+REPLACE INTO spell_script_names VALUES (-1463, "spell_mage_mana_shield"); 
+REPLACE INTO spell_script_names VALUES (37424, "spell_mage_improved_mana_shield");
+REPLACE INTO spell_script_names VALUES (-11119, "spell_mage_ignite");
+REPLACE INTO spell_script_names VALUES (11129, "spell_mage_combustion");
+REPLACE INTO spell_script_names VALUES (28682, "spell_mage_combustion_proc");
+
+DELETE FROM spell_proc WHERE spellid = -1463;
+REPLACE INTO spell_proc (SpellId, ProcFlags, SpellTypeMask, SpellPhaseMask, HitMask, AttributesMask, Chance) VALUES (37424, 0x00100000, 0x1, 0x0000002, 0x0000400, 0x0000002, 20);
+UPDATE spell_proc SET SchoolMask = 0x4, SpellFamilyMask = 0, HitMask = 2 WHERE SpellId = 11129;
