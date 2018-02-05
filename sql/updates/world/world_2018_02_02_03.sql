2630eb1ead97b9383e08c5e4011ced5a9bf81524
Kelno <kelnowm@gmail.com>
2018-02-05 21:08:47 +0100

    Moved rogue and hunter spells from OldWindrunnerHacks to SpellScript + Fixed a bunch of spells using spell_template proc values without spell_proc entry, such as Master Tactician talent from Hunter.
    Former-commit-id: 0abb8820866716b0f7541d4a6a1e03a61432fca9

---
+++ sql/updates/world/world_2018_02_02_03.sql
@@ -0,0 +1,5 @@
+
+REPLACE INTO spell_script_names VALUES (32748, "spell_rog_deadly_throw_interrupt");
+REPLACE INTO spell_script_names VALUES (-31244, "spell_rog_quick_recovery");
+
+REPLACE INTO spell_script_names VALUES (-34497, "spell_hun_thrill_of_the_hunt");
