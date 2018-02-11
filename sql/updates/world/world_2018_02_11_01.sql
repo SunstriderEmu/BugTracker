8e915121b6868c4c307d2b11634d9d36a22fdf03
Kelno <kelnowm@gmail.com>
2018-02-11 05:29:30 +0100

    Cleanup up remaining procs spells from core, finally finished porting all of them to scripts!
    Former-commit-id: ae321ec725e86c93628e327290de526ad67c6f3f

---
+++ sql/updates/world/world_2018_02_11_01.sql
@@ -0,0 +1,5 @@
+REPLACE INTO spell_script_names VALUES (-27811, "spell_pri_blessed_recovery");
+REPLACE INTO `spell_ranks` (`first_spell_id`, `spell_id`, `rank`) VALUES ('27813','27813','1');
+REPLACE INTO `spell_ranks` (`first_spell_id`, `spell_id`, `rank`) VALUES ('27813','27817','2');
+REPLACE INTO `spell_ranks` (`first_spell_id`, `spell_id`, `rank`) VALUES ('27813','27818','3');
+REPLACE INTO spell_script_names VALUES (35095, "spell_gen_enlightenment");
