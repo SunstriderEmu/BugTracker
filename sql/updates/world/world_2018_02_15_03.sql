068ba6ce1fff9801f4d6d52f067b599c770090ca
Kelno <kelnowm@gmail.com>
2018-02-15 11:46:00 +0100

    [TC] DB/SAI: Update SAI that was using threat list target on aggro target_type 2, 3, 4, 5, 6 musn't be used with the event 4
    Former-commit-id: bf3fe4b37c4c52b29b01762324d490d14d3d67fb

---
+++ sql/updates/world/world_2018_02_15_03.sql
@@ -0,0 +1,2 @@
+UPDATE `smart_scripts` SET `target_type`=7 WHERE  `event_type` IN (4) AND `target_type`=2;
+UPDATE `smart_scripts` SET `target_type`=16 WHERE  `event_type` IN (4) AND `target_type`=5;
