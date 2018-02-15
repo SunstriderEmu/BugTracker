56f881c1995d659ca13b0026a4a98b3bf6f058b1
Kelno <kelnowm@gmail.com>
2018-02-15 10:55:57 +0100

    Moved creature permanent feign death spell to script
    Former-commit-id: d0598c7ccd7f59e6cafd8928b5e502bc57d31f01

---
+++ sql/updates/world/world_2018_02_15_01.sql
@@ -0,0 +1,4 @@
+REPLACE INTO spell_script_names VALUES (29266, "spell_gen_creature_permanent_feign_death");
+REPLACE INTO spell_script_names VALUES (31261, "spell_gen_creature_permanent_feign_death");
+REPLACE INTO spell_script_names VALUES (35356, "spell_gen_creature_permanent_feign_death");
+REPLACE INTO spell_script_names VALUES (35357, "spell_gen_creature_permanent_feign_death");
