b32e7990224400fdc4d8264bb34841b325ef4e8c
Kelno <kelnowm@gmail.com>
2018-02-18 20:30:12 +0100

    Added shaman elementals AI
    Former-commit-id: f160c400ed085d4dfdce2d2c63fc740471d16535

---
+++ sql/updates/world/world_2018_02_18_02.sql
@@ -0,0 +1,2 @@
+UPDATE creature_template SET ScriptName = "npc_pet_shaman_earth_elemental" WHERE entry = 15352;
+UPDATE creature_template SET ScriptName = "npc_pet_shaman_fire_elemental" WHERE entry = 15438;
