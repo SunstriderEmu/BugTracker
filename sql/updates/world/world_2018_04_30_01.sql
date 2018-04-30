30a3d22f0176e1308406b90d5efeb0df9c87a6e2
Kelno <kelnowm@gmail.com>
2018-04-30 12:26:20 +0200

    Spells: Fixed druid spell ravage damage and related test + added a bunch of notes about it
    Former-commit-id: 9aac74a19a8f8c7e721694505a82300e0c228399

---
+++ sql/updates/world/world_2018_04_30_01.sql
@@ -0,0 +1,5 @@
+REPLACE INTO spell_template_override (entry, effectBasePoints1, `comment`) VALUE (6785, 37, 'See druid ravage test for explanation');
+REPLACE INTO spell_template_override (entry, effectBasePoints1, `comment`) VALUE (6787, 55, 'See druid ravage test for explanation');
+REPLACE INTO spell_template_override (entry, effectBasePoints1, `comment`) VALUE (9866, 70, 'See druid ravage test for explanation');
+REPLACE INTO spell_template_override (entry, effectBasePoints1, `comment`) VALUE (9867, 88, 'See druid ravage test for explanation');
+REPLACE INTO spell_template_override (entry, effectBasePoints1, `comment`) VALUE (27005, 133, 'See druid ravage test for explanation');
