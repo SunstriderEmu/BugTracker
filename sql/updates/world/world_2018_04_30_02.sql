143df0652331069e2b9e474b64acb14bfe1c9f5f
Kelno <kelnowm@gmail.com>
2018-04-30 13:32:35 +0200

    Spell: Fixed lifebloom spell coefs + related test
    Former-commit-id: 1dbb0c57f1013a1bd436477fa0c66bbcde69fea1

---
+++ sql/updates/world/world_2018_04_30_02.sql
@@ -0,0 +1,4 @@
+REPLACE INTO spell_bonus_data VALUES (33778, 0.343, 0.0, 0.0, 0.0, "Druid - Lifebloom final heal");
+REPLACE INTO spell_bonus_data VALUES (33763, 0.0, 0.07443, 0.0, 0.0, "Druid - Lifebloom ticks");
+
+REPLACE INTO spell_script_names VALUES (-33763, "spell_dru_lifebloom");
