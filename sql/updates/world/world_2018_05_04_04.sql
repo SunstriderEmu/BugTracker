317256137d76c8edb0e01fb38aa3362b55bf222c
Kelno <kelnowm@gmail.com>
2018-05-04 09:44:09 +0200

    Spells: Fixed demonist hellfire spell coef
    Former-commit-id: 52d4363f3d5c246e625786748605059c7331fd56

---
+++ sql/updates/world/world_2018_05_04_04.sql
@@ -0,0 +1,4 @@
+DELETE FROM spell_bonus_data WHERE entry IN (11681, 11682, 27214);
+REPLACE INTO spell_bonus_data VALUES (5857, 0.14, -1, -1, -1, "Hellfire proc spell");
+DELETE FROM spell_bonus_data WHERE entry IN (11683, 11684, 27213);
+REPLACE INTO spell_bonus_data VALUES (1949, -1, 0.1428666, -1, -1, "Hellfire");
