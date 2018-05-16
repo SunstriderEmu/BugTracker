a50490629c34d4664fdc7c87f21a098f08649dfd
Kelno <kelnowm@gmail.com>
2018-05-17 01:18:34 +0200

    Spells: Fixed Priest Chastise threat + Power Word Shield Threat
    Former-commit-id: 83c26f5db2681b7494b928e9750f3337525a3c0b

---
+++ sql/updates/world/world_2018_05_16_02.sql
@@ -0,0 +1,5 @@
+-- Chastise threat
+REPLACE INTO spell_threat VALUES (44041, 0, 0.5);
+
+-- delete pw shield from spell_threat
+DELETE FROM spell_threat WHERE entry IN SELECT spell_id FROM spell_ranks WHERE first_spell_id = 17;
