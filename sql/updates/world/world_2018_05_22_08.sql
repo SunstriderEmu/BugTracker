2cca84f048ac2dd8a979f98a43e058479eaf3664
Kelno <kelnowm@gmail.com>
2018-05-23 00:37:31 +0200

    Spells: Fixed vampiric embrace not stacking for different casters + Testing: Shadowguard stub
    Former-commit-id: fb79d3a8a6b13fa2989db7b9cf8305896a2750ce

---
+++ sql/updates/world/world_2018_05_22_08.sql
@@ -0,0 +1,1 @@
+UPDATE spell_template_override SET AttributesEx3 = 0x4000000 | 0x00000080, Comment = "Add SpellFamilyFlags to allow talent to affect it + Add SPELL_ATTR3_STACK_FOR_DIFF_CASTERS" WHERE entry = 15286;
