3a7d4091dabdabe7913d281d0392abeb79abf464
Kelno <kelnowm@gmail.com>
2018-05-24 00:38:05 +0200

    Spells: Fixed Mass Dispel targeting order + Related test
    Former-commit-id: 104452054a38ff1b7a6a73023842aa27b9a8ea26

---
+++ sql/updates/world/world_2018_05_24_01.sql
@@ -0,0 +1,3 @@
+REPLACE INTO spell_script_names VALUES(32375, "spell_pri_mass_dispel");
+REPLACE INTO spell_script_names VALUES(32592, "spell_pri_mass_dispel_enemies");
+REPLACE INTO spell_script_names VALUES(39897, "spell_pri_mass_dispel_enemies");
